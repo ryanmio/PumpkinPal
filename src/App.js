fae67ec3699e5820d0565c009cd7c0073068d02f
ryanmio <ryan@mioduski.us>
2023-06-11 10:02:07 -0400

    nav style

--- src/App.js
+++ src/App.js
@@ -29,11 +29,11 @@ function App() {
         <header className="App-header">
           {currentUser && (
             <div className="nav-bar">
-              <img src="logo.png" alt="Logo" className="App-logo" />
+              <img src="/logo192.png" alt="Logo" className="App-logo" />
               <Link className="nav-link" to="/dashboard">Dashboard</Link>
               <Link className="nav-link" to="/add-pumpkin">Add Pumpkin</Link>
               <span>User: {currentUser.email}</span>
-              <Logout />
+              <Logout className="logout-button"/>
             </div>
           )}
         </header>
