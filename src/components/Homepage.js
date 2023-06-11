5d4dbd2b3384f2d049d3818619134c905bd651a4
ryanmio <ryan@mioduski.us>
2023-06-11 19:35:27 -0400

    homepage updates

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,6 +1,16 @@
 import React, { useState } from 'react';
-import { useSpring, animated } from 'react-spring';
-import { ParallaxProvider, Parallax } from 'react-scroll-parallax';
+import { useSpring, animated } from 'react-scroll-parallax';
+
+// Define a FeatureCard component
+function FeatureCard({ title, description }) {
+    return (
+        <div className="feature-card">
+            <img src="/logo192.png" alt="Feature" />
+            <h2>{title}</h2>
+            <p>{description}</p>
+        </div>
+    );
+}
 
 export default function Homepage() {
     const [flipped, set] = useState(false);
@@ -14,22 +24,27 @@ export default function Homepage() {
     return (
         <ParallaxProvider>
             <div>
-                <Parallax y={[-20, 20]} tagOuter="figure">
-                    <animated.div
-                        className="c"
-                        onClick={() => set(state => !state)}
-                        style={{
-                            opacity: opacity.interpolate(o => 1 - o),
-                            transform,
-                        }}
-                    >
-                        {/* Your logo or a placeholder image */}
-                        <img src="/logo.png" alt="Logo" />
-                    </animated.div>
-                </Parallax>
+                <div className="hero">
+                    <img src="/hero.png" alt="Hero" />
+                </div>
 
-                {/* Interactive elements, feature showcase, testimonials, and call to action go here */}
-                {/* These will depend heavily on your specific application and design */}
+                <div className="features">
+                    <FeatureCard title="Feature 1" description="Description of Feature 1" />
+                    <FeatureCard title="Feature 2" description="Description of Feature 2" />
+                    <FeatureCard title="Feature 3" description="Description of Feature 3" />
+                    {/* Add more FeatureCard components as needed */}
+                </div>
+
+                <div className="about-us">
+                    <h2>About Us</h2>
+                    <p>This app is an open source work in progress to help pumpkin growers, and is a passion project.</p>
+                </div>
+
+                <div className="join-beta">
+                    <h2>Join the Beta Program</h2>
+                    <p>We need people to offer their regular feedback to improve the app.</p>
+                    <button onClick={() => {/* Add functionality to join the beta program */}}>Join the Beta</button>
+                </div>
             </div>
         </ParallaxProvider>
     );
