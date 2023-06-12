770c5ec1f3bea299d7c4978f73fcd6f5ade948c2
ryanmio <ryan@mioduski.us>
2023-06-12 15:32:17 -0400

    Update UserProfile.js

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,68 +1,52 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { auth, db } from '../firebase';
 import { doc, getDoc, updateDoc } from 'firebase/firestore';
-import { updatePassword, EmailAuthProvider, reauthenticateWithCredential } from 'firebase/auth';
+import { reauthenticateWithCredential, EmailAuthProvider, updatePassword } from 'firebase/auth';
 
 function UserProfile() {
-  const [preferredUnit, setPreferredUnit] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [currentPassword, setCurrentPassword] = useState('');
 
   // Fetch user preferences on component mount
-useEffect(() => {
-  const fetchPreferences = async () => {
+  useEffect(() => {
+    const fetchPreferences = async () => {
+      if (auth.currentUser) {
+        const userRef = doc(db, 'Users', auth.currentUser.uid);
+        const userDoc = await getDoc(userRef);
+        if (userDoc.exists()) {
+          auth.currentUser.preferredUnit = userDoc.data().preferredUnit || 'cm';
+        }
+      }
+    };
+    fetchPreferences();
+  }, []);
+
+  const updatePreferences = async (e) => {
+    e.preventDefault();
     if (auth.currentUser) {
       const userRef = doc(db, 'Users', auth.currentUser.uid);
-      const userDoc = await getDoc(userRef);
-      if (userDoc.exists()) {
-        auth.currentUser.preferredUnit = userDoc.data().preferredUnit || 'cm';
-        setPreferredUnit(auth.currentUser.preferredUnit);
-      }
+      await updateDoc(userRef, { preferredUnit: auth.currentUser.preferredUnit });
+      alert("Preferences updated successfully");
+    } else {
+      alert("User not logged in");
     }
   };
-  fetchPreferences();
-}, []);
-
-const updatePreferences = async (e) => {
-  e.preventDefault();
-  if (auth.currentUser) {
-    const userRef = doc(db, 'Users', auth.currentUser.uid);
-    await updateDoc(userRef, { preferredUnit: auth.currentUser.preferredUnit });
-    alert("Preferences updated successfully");
-  } else {
-    alert("User not logged in");
-  }
-};
-
 
   const handleChangePassword = async (e) => {
     e.preventDefault();
     if (password !== confirmPassword) {
-      alert("New passwords do not match");
+      alert("Passwords do not match");
       return;
     }
-    if(auth.currentUser) {
-      try {
-        // re-authenticate the user
-        const credential = EmailAuthProvider.credential(auth.currentUser.email, currentPassword);
-        await reauthenticateWithCredential(auth.currentUser, credential);
-        
-        // update password
-        await updatePassword(auth.currentUser, password);
-        alert("Password updated successfully");
-      } catch (error) {
-        console.log("Error updating password: ", error);
-        if (error.code === 'auth/wrong-password') {
-          alert("Current password is incorrect");
-        }
-        else if (error.code === 'auth/user-mismatch' || error.code === 'auth/user-not-found') {
-          alert("The current user does not match the requested credential. You may need to login again.");
-        }
-        else {
-          alert("Something went wrong: " + error.message);
-        }
-      }
+    const user = auth.currentUser;
+    const credential = EmailAuthProvider.credential(user.email, currentPassword);
+    try {
+      await reauthenticateWithCredential(user, credential);
+      await updatePassword(user, password);
+      alert("Password updated successfully");
+    } catch (error) {
+      alert("Error updating password: ", error.message);
     }
   };
 
@@ -72,7 +56,7 @@ const updatePreferences = async (e) => {
       <form onSubmit={updatePreferences}>
         <label>
           Preferred Measurement Unit:
-          <select value={auth.currentUser ? auth.currentUser.preferredUnit : 'cm'} onChange={e => setPreferredUnit(e.target.value)}>
+          <select value={auth.currentUser ? auth.currentUser.preferredUnit : 'cm'} onChange={e => auth.currentUser.preferredUnit = e.target.value}>
             <option value="cm">cm</option>
             <option value="in">in</option>
           </select>
@@ -82,15 +66,15 @@ const updatePreferences = async (e) => {
       <form onSubmit={handleChangePassword}>
         <label>
           Current Password:
-          <input type="password" value={currentPassword} onChange={e => setCurrentPassword(e.target.value)} />
+          <input type="password" value={currentPassword} onChange={e => setCurrentPassword(e.target.value)} required />
         </label>
         <label>
           New Password:
-          <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)} required />
         </label>
         <label>
           Confirm New Password:
-          <input type="password" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} />
+          <input type="password" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} required />
         </label>
         <button type="submit">Change Password</button>
       </form>
