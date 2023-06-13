5c26c3c365ba1251e7d9e1563d5de6d3af1a9cd5
ryanmio <ryan@mioduski.us>
2023-06-13 09:38:14 -0400

    Update PumpkinDetail.js

--- src/components/PumpkinDetail.js
+++ src/components/PumpkinDetail.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { auth, db } from '../firebase';
-import { collection, doc, getDoc, query, orderBy } from 'firebase/firestore';
+import { collection, doc, getDoc, query, orderBy, deleteDoc } from 'firebase/firestore';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { Line } from 'react-chartjs-2';
 
@@ -37,13 +37,20 @@ function PumpkinDetail() {
     labels: measurements?.map(m => new Date(m.timestamp.seconds * 1000).toLocaleDateString()),
     datasets: [
       {
-        label: 'Estimated Weight over Time',
+        label: 'Estimated Weight over Time (lbs)',
         data: measurements?.map(m => m.estimatedWeight),
         fill: false,
         backgroundColor: 'rgb(75, 192, 192)',
         borderColor: 'rgba(75, 192, 192, 0.2)',
       },
     ],
+  };
+
+  // Function to delete a measurement
+  const deleteMeasurement = async (measurementId) => {
+    if (window.confirm("Are you sure you want to delete this measurement?")) {
+      await deleteDoc(doc(db, 'Users', auth.currentUser.uid, 'Pumpkins', id, 'Measurements', measurementId));
+    }
   };
 
   return (
@@ -61,7 +68,10 @@ function PumpkinDetail() {
             <th>End to End</th>
             <th>Side to Side</th>
             <th>Circumference</th>
+            <th>Measurement Unit</th>
             <th>Estimated Weight</th>
+            <th>Edit</th>
+            <th>Delete</th>
           </tr>
         </thead>
         <tbody>
@@ -71,7 +81,10 @@ function PumpkinDetail() {
               <td>{measurement.endToEnd}</td>
               <td>{measurement.sideToSide}</td>
               <td>{measurement.circumference}</td>
+              <td>{measurement.measurementUnit}</td>
               <td>{measurement.estimatedWeight}</td>
+              <td><button onClick={() => navigate(`/edit-measurement/${id}/${measurement.id}`)}>Edit</button></td>
+              <td><button onClick={() => deleteMeasurement(measurement.id)}>Delete</button></td>
             </tr>
           ))}
         </tbody>
