ca3e8711cf133e114b5e346db3492b6ded6341eb
ryanmio <ryan@mioduski.us>
2023-06-13 10:52:34 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import AddMeasurement from './components/AddMeasurement';
 import PumpkinDetail from './components/PumpkinDetail';
 import Homepage from './components/Homepage';
 import UserProfile from './components/UserProfile';
+import EditMeasurement from './components/EditMeasurement';
 
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
