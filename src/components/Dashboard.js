d2b12761e3862559eda9c6493d021f5a370d92fd
ryanmio <ryan@mioduski.us>
2023-06-17 19:43:11 -0400

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -19,29 +19,28 @@ function Dashboard() {
         let pumpkinsData = [];
 
         for (let pumpkinDoc of snapshot.docs) {
-            let pumpkinData = pumpkinDoc.data();
+          let pumpkinData = pumpkinDoc.data();
 
-            // Query for the latest measurement
-            const measurementsCollection = collection(db, 'Users', user.uid, 'Pumpkins', pumpkinDoc.id, 'Measurements');
-            const measurementsQuery = query(measurementsCollection, orderBy('timestamp', 'desc'), limit(1));
-            const measurementSnapshot = await getDocs(measurementsQuery);
+          // Query for the latest measurement
+          const measurementsCollection = collection(db, 'Users', user.uid, 'Pumpkins', pumpkinDoc.id, 'Measurements');
+          const measurementsQuery = query(measurementsCollection, orderBy('timestamp', 'desc'), limit(1));
+          const measurementSnapshot = await getDocs(measurementsQuery);
 
-            const latestMeasurement = measurementSnapshot.docs[0]?.data() || null;
+          const latestMeasurement = measurementSnapshot.docs[0]?.data() || null;
 
-            // Add latestMeasurement to pumpkinData
-            pumpkinData.latestMeasurement = latestMeasurement;
-            pumpkinsData.push({ ...pumpkinData, id: pumpkinDoc.id });
+          // Add latestMeasurement to pumpkinData
+          pumpkinData.latestMeasurement = latestMeasurement;
+          pumpkinsData.push({ ...pumpkinData, id: pumpkinDoc.id });
         }
 
         setPumpkins(pumpkinsData);
       }
     });
     return () => unsubscribe();
-}, []);
-
+  }, []);
 
   async function deletePumpkin(id) {
-    if (window.confirm("Are you sure you want to delete this pumpkin?")) {
+    if (window.confirm('Are you sure you want to delete this pumpkin?')) {
       setDeletionStatus('Deleting...');
       await deleteDoc(doc(db, 'Users', auth.currentUser.uid, 'Pumpkins', id));
       setPumpkins(pumpkins.filter(pumpkin => pumpkin.id !== id));
@@ -50,50 +49,112 @@ function Dashboard() {
     }
   }
 
-function daysSincePollination(pollinationDateStr) {
-  const pollinationDate = new Date(pollinationDateStr);
-  const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
-  const now = new Date();
-  const diffDays = Math.round(Math.abs((now - pollinationDate) / oneDay)) - 1;
-  return diffDays;
-}
+  function daysSincePollination(pollinationDateStr) {
+    const pollinationDate = new Date(pollinationDateStr);
+    const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+    const now = new Date();
+    const diffDays = Math.round(Math.abs(now - pollinationDate) / oneDay) - 1;
+    return diffDays;
+  }
 
-return (
-  <div className="container mx-auto px-4">
-    <div className="my-8">
-      <h2 className="text-2xl font-bold mb-2">Welcome to your Dashboard</h2>
-      {email ? (
-        <>
-          <p className="mb-4">Logged in as {email}</p>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={() => auth.signOut()}>Logout</button>
-        </>
-      ) : (
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={() => navigate("/login")}>Login</button>
-      )}
-    </div>
-    <div className="my-8">
-      {deletionStatus && <p className="mb-4">{deletionStatus}</p>}
-      {pumpkins.map(pumpkin => (
-        <div className="bg-white shadow overflow-hidden sm:rounded-lg mb-4" key={pumpkin.id}>
-          <div className="px-4 py-5 sm:px-6">
-            <h3 className="text-lg leading-6 font-medium text-gray-900" onClick={() => navigate(`/pumpkin/${pumpkin.id}`)}>{pumpkin.name}</h3>
-            <p className="mt-1 max-w-2xl text-sm text-gray-500">{pumpkin.description}</p>
-            {pumpkin.latestMeasurement && <p className="mt-1 max-w-2xl text-sm text-gray-500">Latest Weight: {pumpkin.latestMeasurement.estimatedWeight} lbs</p>}
-            {pumpkin.pollinated && <p className="mt-1 max-w-2xl text-sm text-gray-500">Days After Pollination: {daysSincePollination(pumpkin.pollinated)} days</p>}
-            <div className="mt-4">
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-2" onClick={() => navigate(`/add-measurement/${pumpkin.id}`)}>Add Measurement</button>
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-2" onClick={() => navigate(`/edit-pumpkin/${pumpkin.id}`)}>Edit Details</button>
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-2" onClick={() => navigate(`/pumpkin/${pumpkin.id}`)}>Open Detailed View</button>
-              <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={() => deletePumpkin(pumpkin.id)}>Delete</button>
+  return (
+    <div className="container mx-auto px-4">
+      <div className="my-8">
+        <h2 className="text-2xl font-bold mb-2">Welcome to your Dashboard</h2>
+        {email ? (
+          <>
+            <p className="mb-4">Logged in as {email}</p>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              onClick={() => auth.signOut()}
+            >
+              Logout
+            </button>
+          </>
+        ) : (
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            onClick={() => navigate('/login')}
+          >
+            Login
+          </button>
+        )}
+      </div>
+      <div className="my-8">
+        {deletionStatus && <p className="mb-4">{deletionStatus}</p>}
+        {pumpkins.map(pumpkin => (
+          <div
+            className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-4"
+            key={pumpkin.id}
+          >
+            <div className="flex justify-end px-4 pt-4">
+              <button className="text-gray-500 hover:bg-gray-100 focus:outline-none p-1.5">
+                <span className="sr-only">Open dropdown</span>
+                <svg
+                  className="w-6 h-6"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path>
+                </svg>
+              </button>
+            </div>
+            <div className="p-6">
+              <h5 className="mb-2 text-xl font-medium leading-tight text-gray-800">
+                {pumpkin.name}
+              </h5>
+              <p className="mb-4 text-base text-gray-600">
+                {pumpkin.description}
+              </p>
+              <p className="mb-4 text-sm text-gray-600">
+                Maternal Lineage: {pumpkin.maternalLineage}
+              </p>
+              <p className="mb-4 text-sm text-gray-600">
+                Paternal Lineage: {pumpkin.paternalLineage}
+              </p>
+              {pumpkin.latestMeasurement && (
+                <p className="mb-4 text-lg text-gray-800">
+                  Latest Weight: {pumpkin.latestMeasurement.estimatedWeight} lbs
+                </p>
+              )}
+              {pumpkin.pollinated && (
+                <p className="mb-4 text-sm text-gray-600">
+                  Pollinated Date: {pumpkin.pollinated}
+                </p>
+              )}
+              {pumpkin.daysAfterPollination && (
+                <p className="mb-4 text-sm text-gray-600">
+                  Days After Pollination: {pumpkin.daysAfterPollination} days
+                </p>
+              )}
+              <div className="mt-4">
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-2"
+                  onClick={() =>
+                    navigate(`/add-measurement/${pumpkin.id}`)
+                  }
+                >
+                  Add Measurement
+                </button>
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-2"
+                  onClick={() => navigate(`/pumpkin/${pumpkin.id}`)}
+                >
+                  Open Detailed View
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full" onClick={() => navigate('/add-pumpkin')}>Add Pumpkin</button>
+        ))}
+      </div>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
+        onClick={() => navigate('/add-pumpkin')}
+      >
+        Add Pumpkin
+      </button>
     </div>
-  </div>
-);
-
+  );
 }
 
 export default Dashboard;
