675c7f9e605fb6ee1c4f25a4c2afb60ef4e1002d
ryanmio <ryan@mioduski.us>
2023-06-17 21:51:58 -0400

    Update Dropdown.js

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,5 @@
 import { Fragment } from 'react';
 import { Menu, Transition } from '@headlessui/react';
-import { EllipsisVerticalIcon } from '@heroicons/react/solid';
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
@@ -11,7 +10,9 @@ function Dropdown({ onEdit, onDelete }) {
     <Menu as="div" className="relative inline-block text-left">
       <div>
         <Menu.Button className="inline-flex justify-center w-full rounded-md bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-          <EllipsisVerticalIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
+          </svg>
         </Menu.Button>
       </div>
 
