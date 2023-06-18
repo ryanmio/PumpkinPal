1181e744d719b2dae35ba5c36624d96c515d49bc
ryanmio <ryan@mioduski.us>
2023-06-17 21:24:26 -0400

    Update Dropdown.js

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,48 +1,61 @@
-import React, { useState, useRef, useEffect } from 'react';
+import { Fragment } from 'react';
+import { Menu, Transition } from '@headlessui/react';
+import { ChevronDownIcon } from '@heroicons/react/20/solid';
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
 
 function Dropdown({ onEdit, onDelete }) {
-  const [isOpen, setIsOpen] = useState(false);
-  const toggleRef = useRef(null);
-  const dropdownRef = useRef(null);
-
-  // Click outside handler
-  useEffect(() => {
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, []);
-
-  const handleClickOutside = (event) => {
-    if (toggleRef.current && !toggleRef.current.contains(event.target) && !dropdownRef.current.contains(event.target)) {
-      setIsOpen(false);
-    }
-  }
-
   return (
-    <div className="relative inline-block text-left">
+    <Menu as="div" className="relative inline-block text-left">
       <div>
-        <button ref={toggleRef} type="button" onClick={() => setIsOpen(!isOpen)} className="inline-flex justify-center w-7 h-7 rounded-full bg-gray-400 hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500" id="options-menu" aria-haspopup="true" aria-expanded="true">
-          <span className="sr-only">Open options</span>
-          {/* Icon when menu is closed. */}
-          <svg className={`${isOpen ? 'hidden' : 'block'} h-5 w-5`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
-          </svg>
-          {/* Icon when menu is open. */}
-          <svg className={`${isOpen ? 'block' : 'hidden'} h-5 w-5`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-          </svg>
-        </button>
+        <Menu.Button className="inline-flex justify-center w-full rounded-md bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+          <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+        </Menu.Button>
       </div>
-      {isOpen && (
-        <div ref={dropdownRef} className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-            <button type="button" className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem" onClick={onEdit}>Edit</button>
-            <button type="button" className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem" onClick={onDelete}>Delete</button>
+            <Menu.Item>
+              {({ active }) => (
+                <button
+                  onClick={onEdit}
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block w-full text-left px-4 py-2 text-sm'
+                  )}
+                >
+                  Edit
+                </button>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <button
+                  onClick={onDelete}
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block w-full text-left px-4 py-2 text-sm'
+                  )}
+                >
+                  Delete
+                </button>
+              )}
+            </Menu.Item>
           </div>
-        </div>
-      )}
-    </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
   );
 }
 
