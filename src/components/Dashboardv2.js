6adb6d33e739ab33f85a62571b86def91f53fc3c
ryanmio <ryan@mioduski.us>
2023-06-18 13:37:12 -0400

    svgs as components

--- src/components/Dashboardv2.js
+++ src/components/Dashboardv2.js
@@ -5,23 +5,10 @@ import { useNavigate } from 'react-router-dom';
 import { collection, doc, getDocs, deleteDoc } from 'firebase/firestore';
 import Dropdown from './Dropdown';
 import Spinner from './Spinner';
+import CalendarIcon from './icons/CalendarIcon';
+import PlusIcon from './icons/PlusIcon';
+import TableCellsIcon from './icons/TableCellsIcon';
 
-// SVGs
-function PlusIcon() {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-      <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
-    </svg>
-  );
-}
-
-function TableCellsIcon() {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-      <path fillRule="evenodd" d="M.99 5.24A2.25 2.25 0 013.25 3h13.5A2.25 2.25 0 0119 5.25l.01 9.5A2.25 2.25 0 0116.76 17H3.26A2.267 2.267 0 011 14.74l-.01-9.5zm8.26 9.52v-.625a.75.75 0 00-.75-.75H3.25a.75.75 0 00-.75.75v.615c0 .414.336.75.75.75h5.373a.75.75 0 00.627-.74zm1.5 0a.75.75 0 00.627.74h5.373a.75.75 0 00.75-.75v-.615a.75.75 0 00-.75-.75H11.5a.75.75 0 00-.75.75v.625zm6.75-3.63v-.625a.75.75 0 00-.75-.75H11.5a.75.75 0 00-.75.75v.625c0 .414.336.75.75.75h5.25a.75.75 0 00.75-.75zm-8.25 0v-.625a.75.75 0 00-.75-.75H3.25a.75.75 0 00-.75.75v.625c0 .414.336.75.75.75H8.5a.75.75 0 00.75-.75zM17.5 7.5v-.625a.75.75 0 00-.75-.75H11.5a.75.75 0 00-.75.75V7.5c0 .414.336.75.75.75h5.25a.75.75 0 00.75-.75zm-8.25 0v-.625a.75.75 0 00-.75-.75H3.25a.75.75 0 00-.75.75V7.5c0 .414.336.75.75.75H8.5a.75.75 0 00.75-.75z" clipRule="evenodd" />
-    </svg>
-  );
-}
 
 function Dashboard() {
   const [email, setEmail] = useState('');
@@ -119,12 +106,12 @@ return (
                   <div className="w-full grid grid-cols-2 gap-2">
                     <button className="inline-flex items-center justify-center px-2 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                       onClick={() => navigate(`/add-measurement/${pumpkin.id}`)}>
-                      <div className="w-4 h-4 mr-2 flex items-center"><PlusIcon /></div>
+                      <PlusIcon className="w-4 h-4 mr-2" />
                       Add Measurement
                     </button>
                     <button className="inline-flex items-center justify-center px-2 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                       onClick={() => navigate(`/pumpkin/${pumpkin.id}`)}>
-                      <div className="w-4 h-4 mr-2 flex items-center"><TableCellsIcon /></div>
+                      <TableCellsIcon className="w-4 h-4 mr-2" />
                       Detailed View
                     </button>
                   </div>
@@ -140,6 +127,7 @@ return (
     )}
   </div>
 );
+
 
 }
 
