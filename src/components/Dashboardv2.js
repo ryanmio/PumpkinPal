e1d6e466b6a0b03547739fc3f5075a2bc09f98b9
ryanmio <ryan@mioduski.us>
2023-06-18 12:10:56 -0400

    Update Dashboardv2.js

--- src/components/Dashboardv2.js
+++ src/components/Dashboardv2.js
@@ -119,12 +119,12 @@ return (
               <div className="w-full grid grid-cols-2 gap-2">
                 <button className="inline-flex items-center justify-center px-2 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                   onClick={() => navigate(`/add-measurement/${pumpkin.id}`)}>
-                  <div className="w-4 h-4 mr-2"><PlusIcon /></div>
+                  <div className="w-4 h-4 mr-2 flex items-center"><PlusIcon /></div>
                   Add Measurement
                 </button>
                 <button className="inline-flex items-center justify-center px-2 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                   onClick={() => navigate(`/pumpkin/${pumpkin.id}`)}>
-                  <div className="w-4 h-4 mr-2"><TableCellsIcon /></div>
+                  <div className="w-4 h-4 mr-2 flex items-center"><TableCellsIcon /></div>
                   Detailed View
                 </button>
               </div>
