15cfa0dc47e78da0bb9c17dce82d1e89edf6315b
ryanmio <ryan@mioduski.us>
2023-06-19 20:58:46 -0400

    Update Homepage.js

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -6,16 +6,16 @@ export default function Homepage() {
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-200">
 
         {/* Hero Section */}
-        <div className="flex items-center justify-center w-full h-auto py-20 pr-10">
-            <div className="relative mx-auto border-gray-800 bg-gray-800 border-[14px] rounded-[2.5rem] h-[600px] w-[300px] shadow-xl overflow-hidden">
-                <img src="/images/screenmock-details-mobile.png" alt="App mockup" className="absolute w-full h-full object-cover" />
-            </div>
-            <div className="ml-10">
-                <h1 className="text-4xl font-bold mb-4">PumpkinPal</h1>
-                <p className="text-xl mb-6">An open-source companion app for pumpkin growers</p>
-                <Link to="/signup" className="px-8 py-4 green-button rounded text-white text-xl text-white hover:text-white focus:outline-none focus:ring-0 underline-none">Create Account</Link>
-            </div>
-        </div>
+<div className="flex items-center justify-center w-full h-auto py-20 pr-10">
+    <div className="relative mx-auto border-gray-800 bg-gray-800 border-[14px] rounded-[2.5rem] h-[600px] w-[300px] shadow-xl overflow-hidden">
+        <img src="/images/screenmock-details-mobile.png" alt="App mockup" className="absolute w-full h-full object-cover" />
+    </div>
+    <div className="ml-10">
+        <h1 className="text-4xl font-bold mb-4">PumpkinPal</h1>
+        <p className="text-xl mb-6">An open-source companion app for pumpkin growers</p>
+        <Link to="/signup" className="px-8 py-4 green-button rounded text-white text-xl text-white hover:text-white focus:outline-none focus:ring-0 underline-none no-underline font-bold">Create Account</Link>
+    </div>
+</div>
 
 
             {/* Features Section */}
@@ -35,12 +35,12 @@ export default function Homepage() {
                 <img className="w-full h-auto rounded-lg" src="/images/screenmockup-details.png" alt="App screenshot" />
             </div>
 
-         {/* Call to Action Section */}
-        <div className="w-full p-8">
-            <p className="mb-6">Sign up to start tracking your pumpkins. We respect your privacy and do not share your data.</p>
-            <Link to="/signup" className="px-8 py-2 my-6 green-button rounded text-white text-xl text-white hover:text-white focus:outline-none focus:ring-0 underline-none">Sign Up</Link>
-            <p>Already signed up? <Link to="/login" className="text-blue-500">Login here</Link></p>
-        </div>
+        {/* Call to Action Section */}
+<div className="w-full p-8">
+    <p className="mb-6">Sign up to start tracking your pumpkins. We respect your privacy and do not share your data.</p>
+    <Link to="/signup" className="px-8 py-2 my-6 green-button rounded text-white text-xl text-white hover:text-white focus:outline-none focus:ring-0 underline-none no-underline font-bold">Sign Up</Link>
+    <p>Already signed up? <Link to="/login" className="text-blue-500">Login here</Link></p>
+</div>
 
 
 
