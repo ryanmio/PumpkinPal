f5b2edc3fdcfe0e0d1b0b0d078cdecbe8f0e19be
ryanmio <ryan@mioduski.us>
2023-06-20 18:35:57 -0400

    Update Homepage.js

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -42,11 +42,10 @@ export default function Homepage() {
 </div>
 
 {/* Screenshots Section */}
-<div className="w-full px-8">
-    <div className="p-6">
-        <img className="w-full h-auto rounded-lg" src="/images/screenmockup-details.png" alt="App screenshot" />
-    </div>
+<div className="w-full px-4 sm:px-6 md:px-14">
+    <img className="w-full h-auto rounded-lg" src="/images/screenmockup-details.png" alt="App screenshot" />
 </div>
+
 
        {/* Call to Action Section */}
 <div className="w-full p-8">
