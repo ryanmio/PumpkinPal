c3d330de10a46c20bf38312b4891f6b8b97bf6b9
ryanmio <ryan@mioduski.us>
2023-06-20 11:44:03 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,33 +3,29 @@ import { Link, useLocation } from 'react-router-dom';
 import Logout from './Logout';
 import Login from './Login';
 
-
 function Header({ currentUser }) {
   const location = useLocation();
+  
   return (
-    <header className="App-header">
-      <div className="nav-bar">
-        <div className="nav-row">
-          {currentUser ? (
-            <>
-              <img src="/logo192.png" alt="Logo" className="App-logo" />
-              <span>User: {currentUser.email}</span>
-              <Logout className="logout-button"/>
-            </>
-          ) : (
-            location.pathname === '/' && (
-              <Link to="/login" className="logout-button">Login</Link>
-            )
-          )}
-        </div>
-        {currentUser && (
-          <div className="nav-row">
-            <Link className="nav-link" to="/dashboard">Dashboard</Link>
-            <Link className="nav-link" to="/user-profile">User Profile</Link>
-          </div>
+    <div className="nav-bar">
+      <div className="nav-row">
+        {location.pathname === "/" && !currentUser ? (
+          <Link to="/login" className="logout-button">Login</Link>
+        ) : (
+          <>
+            <img src="/logo192.png" alt="Logo" className="App-logo" />
+            <span>User: {currentUser ? `User: ${currentUser.email}` : ''}</span>
+            {currentUser && <Logout className="logout-button"/>}
+          </>
         )}
       </div>
-    </header>
+      {currentUser && (
+        <div className="nav-row">
+          <Link className="nav-link" to="/dashboard">Dashboard</Link>
+          <Link className="nav-link" to="/user-profile">User Profile</Link>
+        </div>
+      )}
+    </div>
   );
 }
 
