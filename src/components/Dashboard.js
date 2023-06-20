2f65eec84f982402b47a2d6c8d1273d899764675
ryanmio <ryan@mioduski.us>
2023-06-20 16:54:52 -0400

    dashboard full length background color

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -66,7 +66,7 @@ function Dashboard() {
   }
 
 return (
-  <div className="container mx-auto px-4">
+  <div className="container mx-auto px-4 h-screen">
     <div className="my-8">
       <h2 className="text-2xl font-bold mb-2">Welcome to your Dashboard</h2>
       {!email && (
