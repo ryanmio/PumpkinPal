17e6241d90331c00babd0160bbcfba15c1b1908c
ryanmio <ryan@mioduski.us>
2023-06-20 11:46:16 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,25 +7,27 @@ function Header({ currentUser }) {
   const location = useLocation();
   
   return (
-    <div className="nav-bar">
-      <div className="nav-row">
-        {location.pathname === "/" && !currentUser ? (
-          <Link to="/login" className="logout-button">Login</Link>
-        ) : (
-          <>
-            <img src="/logo192.png" alt="Logo" className="App-logo" />
-            <span>User: {currentUser ? `User: ${currentUser.email}` : ''}</span>
-            {currentUser && <Logout className="logout-button"/>}
-          </>
+    <header className="App-header">
+      <div className="nav-bar">
+        <div className="nav-row">
+          {location.pathname === "/" && !currentUser ? (
+            <Link to="/login" className="logout-button">Login</Link>
+          ) : (
+            <>
+              <img src="/logo192.png" alt="Logo" className="App-logo" />
+              <span>User: {currentUser ? `User: ${currentUser.email}` : ''}</span>
+              {currentUser && <Logout className="logout-button"/>}
+            </>
+          )}
+        </div>
+        {currentUser && (
+          <div className="nav-row">
+            <Link className="nav-link" to="/dashboard">Dashboard</Link>
+            <Link className="nav-link" to="/user-profile">User Profile</Link>
+          </div>
         )}
       </div>
-      {currentUser && (
-        <div className="nav-row">
-          <Link className="nav-link" to="/dashboard">Dashboard</Link>
-          <Link className="nav-link" to="/user-profile">User Profile</Link>
-        </div>
-      )}
-    </div>
+    </header>
   );
 }
 
