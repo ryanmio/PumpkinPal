6e4ba134866d6a1a1d09081bc73f0b8d3e7f8cf0
ryanmio <ryan@mioduski.us>
2023-06-23 13:19:50 -0400

    bugs

--- src/components/AddMeasurement.js
+++ src/components/AddMeasurement.js
@@ -168,11 +168,12 @@ const calculateOTT = () => {
          <div className="flex justify-between items-center mt-4">
           <button type="button" onClick={() => navigate('/dashboard')} className="text-blue-600 hover:underline">Cancel</button>
           <button 
-              type="submit" 
-              className={`inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 ${endToEnd && sideToSide && circumference ? 'bg-green-500 text-white hover:bg-green-600 focus:ring-green-500' : 'bg-gray-300 text-gray-500 hover:bg-gray-400 cursor-not-allowed'}`}
-            >
-              {calculateOTT() !== 0 ? `Save (OTT = ${calculateOTT()})` : 'Save Measurement'}
-            </button>
+          type="submit" 
+          className={`inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 ${endToEnd && sideToSide && circumference ? 'bg-green-500 text-white hover:bg-green-600 focus:ring-green-500' : 'button-disabled'}`}
+        >
+          {calculateOTT() !== 0 ? `Save (OTT = ${calculateOTT()})` : 'Save Measurement'}
+        </button>
+
         </div>
         </form>
       </div>
