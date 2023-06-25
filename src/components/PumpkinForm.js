ef6f8ba4478c5eb3e8188ee068c6582b8d6c30b9
ryanmio <ryan@mioduski.us>
2023-06-25 19:35:23 -0400

    Update PumpkinForm.js

--- src/components/PumpkinForm.js
+++ src/components/PumpkinForm.js
@@ -42,43 +42,51 @@ function PumpkinForm() {
     }
   };
 
-  return (
+ return (
     <div className="container mx-auto px-4 h-screen pt-10">
       <div className="bg-white shadow overflow-hidden rounded-lg p-6 w-full md:max-w-md mx-auto">
         <h2 className="text-2xl font-bold mb-2 text-center">Add a Pumpkin</h2>
         {error && <p className="text-red-600">{error}</p>}
         <form onSubmit={addPumpkin} className="space-y-4">
-          <label className="block text-left">Name:</label>
-          <input type="text" placeholder="Bear Swipe" value={name} onChange={(e) => setName(e.target.value)} required className="w-full p-2 border-2 border-gray-300 rounded" />
+          <div className="field space-y-1">
+            <label className="block text-left">Name:</label>
+            <input type="text" placeholder="Bear Swipe" value={name} onChange={(e) => setName(e.target.value)} required className="w-full p-2 border-2 border-gray-300 rounded" />
+          </div>
           
-          <label className="block text-left">Description:</label>
-          <textarea placeholder="150 patch" value={description} onChange={(e) => setDescription(e.target.value)} className="w-full p-2 border-2 border-gray-300 rounded"></textarea>
+          <div className="field space-y-1">
+            <label className="block text-left">Description:</label>
+            <textarea placeholder="150 patch" value={description} onChange={(e) => setDescription(e.target.value)} className="w-full p-2 border-2 border-gray-300 rounded"></textarea>
+          </div>
           
-          <label className="block text-left">Maternal Lineage:</label>
-          <input type="text" placeholder="1375 Connolly" value={maternalLineage} onChange={(e) => setMaternalLineage(e.target.value)} className="w-full p-2 border-2 border-gray-300 rounded" />
+          <div className="field space-y-1">
+            <label className="block text-left">Maternal Lineage:</label>
+            <input type="text" placeholder="1375 Connolly" value={maternalLineage} onChange={(e) => setMaternalLineage(e.target.value)} className="w-full p-2 border-2 border-gray-300 rounded" />
+          </div>
 
-          <label className="block text-left">Paternal Lineage:</label>
-          <input type="text" placeholder="1676 New" value={paternalLineage} onChange={(e) => setPaternalLineage(e.target.value)} className="w-full p-2 border-2 border-gray-300 rounded" />
+          <div className="field space-y-1">
+            <label className="block text-left">Paternal Lineage:</label>
+            <input type="text" placeholder="1676 New" value={paternalLineage} onChange={(e) => setPaternalLineage(e.target.value)} className="w-full p-2 border-2 border-gray-300 rounded" />
+          </div>
           
-          <div className="grid grid-cols-2 gap-1">
-          <label className="text-left">Seed Started:</label>
-          <DatePicker selected={seedStarted} onChange={(date) => setSeedStarted(date)} isClearable placeholderText="Select Date" className="w-full p-2 border-2 border-gray-300 rounded" />
-        </div>
+          <div className="grid grid-cols-2 gap-1 field space-y-1">
+            <label className="text-left">Seed Started:</label>
+            <DatePicker selected={seedStarted} onChange={(date) => setSeedStarted(date)} isClearable placeholderText="Select Date" className="w-full p-2 border-2 border-gray-300 rounded" />
+          </div>
         
-        <div className="grid grid-cols-2 gap-1">
-          <label className="text-left">Transplant Out:</label>
-          <DatePicker selected={transplantOut} onChange={(date) => setTransplantOut(date)} isClearable placeholderText="Select Date" className="w-full p-2 border-2 border-gray-300 rounded" />
-        </div>
+          <div className="grid grid-cols-2 gap-1 field space-y-1">
+            <label className="text-left">Transplant Out:</label>
+            <DatePicker selected={transplantOut} onChange={(date) => setTransplantOut(date)} isClearable placeholderText="Select Date" className="w-full p-2 border-2 border-gray-300 rounded" />
+          </div>
         
-        <div className="grid grid-cols-2 gap-1">
-          <label className="text-left">Pollinated:</label>
-          <DatePicker selected={pollinated} onChange={(date) => setPollinated(date)} isClearable placeholderText="Select Date" className="w-full p-2 border-2 border-gray-300 rounded" />
-        </div>
+          <div className="grid grid-cols-2 gap-1 field space-y-1">
+            <label className="text-left">Pollinated:</label>
+            <DatePicker selected={pollinated} onChange={(date) => setPollinated(date)} isClearable placeholderText="Select Date" className="w-full p-2 border-2 border-gray-300 rounded" />
+          </div>
         
-        <div className="grid grid-cols-2 gap-1">
-          <label className="text-left">Weigh-Off:</label>
-          <DatePicker selected={weighOff} onChange={(date) => setWeighOff(date)} isClearable placeholderText="Select Date" className="w-full p-2 border-2 border-gray-300 rounded" />
-        </div>
+          <div className="grid grid-cols-2 gap-1 field space-y-1">
+            <label className="text-left">Weigh-Off:</label>
+            <DatePicker selected={weighOff} onChange={(date) => setWeighOff(date)} isClearable placeholderText="Select Date" className="w-full p-2 border-2 border-gray-300 rounded" />
+          </div>
 
           <div className="flex justify-between items-center mt-4">
             <button type="button" onClick={() => navigate('/dashboard')} className="text-blue-600 hover:underline">Cancel</button>
