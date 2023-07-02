9c08ee934bb180b96e0c2405e837f04b5370b9e1
ryanmio <ryan@mioduski.us>
2023-07-02 15:01:45 -0400

    Update PumpkinDetail.js

--- src/components/PumpkinDetail.js
+++ src/components/PumpkinDetail.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate, Link } from 'react-router-dom';
+import { useParams, useNavigate, Link, useLocation } from 'react-router-dom';
 import { auth, db } from '../firebase';
 import { collection, doc, getDoc, orderBy, onSnapshot, query } from 'firebase/firestore';
 import MeasurementsCard from './MeasurementsCard';
