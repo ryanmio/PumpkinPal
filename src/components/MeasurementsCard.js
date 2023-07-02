59a98804881b3f10d7d448c632467b411f228ba4
ryanmio <ryan@mioduski.us>
2023-07-01 21:24:07 -0400

    Update MeasurementsCard.js

--- src/components/MeasurementsCard.js
+++ src/components/MeasurementsCard.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { auth, db } from '../firebase';
-import { doc, deleteDoc } from 'firebase/firestore';
 
 const MeasurementsCard = ({ measurements, pumpkinId, deleteMeasurement, exportData, alert }) => {
   const navigate = useNavigate();
