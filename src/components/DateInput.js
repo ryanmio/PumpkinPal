9ec1961a6e1e610e6f6c4c5e8ed6e566e866da13
ryanmio <ryan@mioduski.us>
2023-07-02 11:36:53 -0400

    Update DateInput.js

--- src/components/DateInput.js
+++ src/components/DateInput.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
+import { toast } from 'react-hot-toast';
 
-function CustomInput({ value, onClick, className, onChange }) {
+function CustomInput({ value, onClick, className, onBlur, onChange }) {
   const id = "custom-date-input"; // or any other unique id
   const isToday = value && new Date(value).toDateString() === new Date().toDateString();
   
@@ -12,6 +13,7 @@ function CustomInput({ value, onClick, className, onChange }) {
         id={id}
         className={`input text-center text-2xl w-full p-2 border-2 border-gray-300 rounded ${value ? "filled" : ""}`}
         onClick={onClick}
+        onBlur={onBlur}
         onChange={onChange}
         value={value}
       />
@@ -21,22 +23,31 @@ function CustomInput({ value, onClick, className, onChange }) {
 }
 
 function DateInput({ selected, onChange }) {
-  const handleChangeRaw = (e) => {
-    const date = moment(e.target.value, 'MM/DD/YYYY', true);
-    
-    if (date.isValid()) {
-      onChange(date.toDate());
+  const [inputValue, setInputValue] = useState(moment(selected).format("MM/DD/YYYY"));
+  
+  const handleBlur = (e) => {
+    const value = e.target.value;
+    if (!moment(value, "MM/DD/YYYY", true).isValid()) {
+      toast.error("Invalid date. Please use MM/DD/YYYY format.");
     }
-  };
+  }
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  }
+  
+  const handleDateChange = (date) => {
+    setInputValue(moment(date).format("MM/DD/YYYY"));
+    onChange(date);
+  }
 
   return (
     <DatePicker 
       selected={selected}
-      onChange={onChange}
-      onChangeRaw={handleChangeRaw}
-      dateFormat="MM/dd/yyyy"
+      onChange={handleDateChange}
       required
-      customInput={<CustomInput onClick={handleChangeRaw} />}
+      customInput={<CustomInput onBlur={handleBlur} onChange={handleInputChange} />}
+      dateFormat="MM/dd/yyyy"
     />
   );
 }
