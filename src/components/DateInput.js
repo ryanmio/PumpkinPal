9a34724469c0cad8ba5eaa88dc424817314a753a
ryanmio <ryan@mioduski.us>
2023-07-02 11:31:35 -0400

    typing in date input

--- src/components/DateInput.js
+++ src/components/DateInput.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import DatePicker from 'react-datepicker';
+import moment from 'moment';
 
-function CustomInput({ value, onClick, className }) {
+function CustomInput({ value, onClick, className, onChange }) {
   const id = "custom-date-input"; // or any other unique id
   const isToday = value && new Date(value).toDateString() === new Date().toDateString();
   
@@ -11,6 +12,7 @@ function CustomInput({ value, onClick, className }) {
         id={id}
         className={`input text-center text-2xl w-full p-2 border-2 border-gray-300 rounded ${value ? "filled" : ""}`}
         onClick={onClick}
+        onChange={onChange}
         value={value}
       />
       <label htmlFor={id} className={`absolute left-0 top-0 label-float ${value ? "filled" : ""}`}>{isToday ? "Date (Today)" : "Date"}</label>
@@ -19,12 +21,22 @@ function CustomInput({ value, onClick, className }) {
 }
 
 function DateInput({ selected, onChange }) {
+  const handleChangeRaw = (e) => {
+    const date = moment(e.target.value, 'MM/DD/YYYY', true);
+    
+    if (date.isValid()) {
+      onChange(date.toDate());
+    }
+  };
+
   return (
     <DatePicker 
       selected={selected}
       onChange={onChange}
+      onChangeRaw={handleChangeRaw}
+      dateFormat="MM/dd/yyyy"
       required
-      customInput={<CustomInput />}
+      customInput={<CustomInput onClick={handleChangeRaw} />}
     />
   );
 }
