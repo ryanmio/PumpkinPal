71d955028818997e91cf6ef7abd8e7e4bc2ab5dc
ryanmio <ryan@mioduski.us>
2023-07-01 21:16:19 -0400

    Refactor attempt 2

--- src/components/PumpkinDetail.js
+++ src/components/PumpkinDetail.js
@@ -3,6 +3,8 @@ import { useParams, useNavigate, useLocation, Link } from 'react-router-dom';
 import { auth, db } from '../firebase';
 import { collection, doc, getDoc, query, orderBy, deleteDoc, onSnapshot } from 'firebase/firestore';
 import { Line } from 'react-chartjs-2';
+import MeasurementsCard from './MeasurementsCard';
+import GraphCard from './GraphCard';
 
 function PumpkinDetail() {
   const { id } = useParams();
@@ -160,52 +162,21 @@ return (
           <button onClick={() => navigate(`/edit-pumpkin/${id}`, { state: { from: location.pathname } })} className="green-button inline-flex items-center justify-center px-2 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 mt-4 self-end">Edit Dates</button>
         </div>
 
-{/* Card 3: Measurements */}
-<div className="bg-white shadow rounded-lg p-4 md:col-span-2 flex flex-col overflow-x-auto">
-    <h3 className="text-xl font-bold mb-2">Measurements</h3>
-    <div className="flex space-x-4 justify-center">
-        <button onClick={() => navigate(`/add-measurement/${id}`)} className="green-button inline-flex items-center justify-center px-2 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">Add Measurement</button>
-        <button onClick={exportData} className="green-button inline-flex items-center justify-center px-2 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">Export Data</button>
-    </div>
-    {alert && <div className={`alert ${alert.type}`}>{alert.message}</div>}
-    <div className="overflow-x-auto">
-        <table className="w-full mt-4">
-            <thead>
-                <tr>
-                    <th className="whitespace-nowrap min-w-max w-[100px] table-cell">Date</th>
-                    <th className="table-cell">End to End</th>
-                    <th className="table-cell">Side to Side</th>
-                    <th className="table-cell">Circ.</th>
-                    <th className="table-cell">Units</th>
-                    <th className="table-cell">OTT Weight</th>
-                    <th>Edit</th>
-                    <th>Delete</th>
-                </tr>
-            </thead>
-            <tbody>
-                {measurements && measurements.map(measurement => (
-                    <tr key={measurement.id}>
-                        <td className="whitespace-nowrap table-cell">{measurement.timestamp}</td>
-                        <td className="table-cell">{measurement.endToEnd}</td>
-                        <td className="table-cell">{measurement.sideToSide}</td>
-                        <td className="table-cell">{measurement.circumference}</td>
-                        <td className="table-cell">{measurement.measurementUnit}</td>
-                        <td className="table-cell">{measurement.estimatedWeight}</td>
-                        <td><button onClick={() => navigate(`/edit-measurement/${id}/${measurement.id}`)} className="green-button inline-flex items-center justify-center px-2 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">Edit</button></td>
-                        <td><button onClick={() => deleteMeasurement(measurement.id)} className="green-button inline-flex items-center justify-center px-2 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">Delete</button></td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-    </div>
-</div>
+ {/* Card 3: Measurements */}
+    <MeasurementsCard
+      measurements={measurements}
+      alert={alert}
+      deleteMeasurement={deleteMeasurement}
+      navigate={navigate}
+      pumpkinId={id}
+      setAlert={setAlert}
+    />
 
-
-      {/* Card 4: Graph */}
-      <div className="bg-white shadow rounded-lg p-4 md:col-span-2 mb-10">
-        <h3 className="text-xl font-bold mb-2">Graph</h3>
-        <Line data={chartData} />
-      </div>
+    {/* Card 4: Graph */}
+    <GraphCard
+      measurements={measurements}
+      pumpkinName={pumpkin?.name}
+    />
 
     </div> 
   </div>
