6f98030e7ce09b783dd4911eeb611c6e69b7076b
ryanmio <ryan@mioduski.us>
2023-07-07 17:10:36 -0400

    Update Stats.js

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -6,7 +6,7 @@ import { doc, getDoc } from "firebase/firestore";
 import { IconContext } from "react-icons";
 
 const StatCard = ({ Icon, label, count }) => (
-  <div className="flex flex-col items-center justify-center px-4 py-2 border-r border-gray-200">
+  <div className="flex flex-col items-center justify-center px-4 py-2 border-r border-gray-200 flex-grow flex-shrink">
     <div className="flex items-center justify-center w-10 h-10 mb-3 rounded-full">
       <IconContext.Provider value={{ color: "#80876E", size: "2em" }}>
         <Icon />
@@ -38,10 +38,10 @@ const Stats = () => {
 
   return (
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-      <div className="flex justify-between p-4 bg-white shadow-md rounded-lg">
+      <div className="flex justify-between p-4 bg-white shadow-md rounded-lg max-w-full md:max-w-3xl mx-auto w-full">
         <StatCard Icon={BsPeopleFill} label="Users" count={stats?.userCount} />
         <StatCard Icon={GiPumpkin} label="Pumpkins" count={stats?.pumpkinCount} />
-        <div className="flex flex-col items-center justify-center px-4 py-2">
+        <div className="flex flex-col items-center justify-center px-4 py-2 flex-grow flex-shrink">
           <div className="flex items-center justify-center w-10 h-10 mb-3 rounded-full">
             <IconContext.Provider value={{ color: "#80876E", size: "2em" }}>
               <BsClipboardData />
