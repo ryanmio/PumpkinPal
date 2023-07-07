02499cbbaa27b4af64b20a57dd46e82287ed3036
ryanmio <ryan@mioduski.us>
2023-07-07 17:21:53 -0400

    Update Stats.js

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -6,14 +6,14 @@ import { doc, getDoc } from "firebase/firestore";
 import { IconContext } from "react-icons";
 
 const StatCard = ({ Icon, label, count }) => (
-  <div className="flex flex-col items-center justify-center p-4 border-r border-gray-200">
+  <div className="flex flex-col items-center justify-center p-4">
     <div className="flex items-center justify-center w-10 h-10 mb-3 rounded-full">
       <IconContext.Provider value={{ color: "#80876E", size: "2em" }}>
         <Icon />
       </IconContext.Provider>
     </div>
     <h6 className="text-4xl font-bold">{count !== null ? count : '--'}</h6>
-    <p className="mb-2 font-bold text-md text-center min-w-[6rem]">{label}</p>
+    <p className="mb-2 font-bold text-md text-center">{label}</p>
   </div>
 );
 
@@ -38,7 +38,7 @@ const Stats = () => {
 
   return (
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-      <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 bg-white shadow-md rounded-lg max-w-full md:max-w-3xl mx-auto w-full">
+      <div className="flex justify-between bg-white shadow-md rounded-lg max-w-full md:max-w-3xl mx-auto w-full">
         <StatCard Icon={BsPeopleFill} label="Users" count={stats?.userCount} />
         <StatCard Icon={GiPumpkin} label="Pumpkins" count={stats?.pumpkinCount} />
         <StatCard Icon={BsClipboardData} label="Measurements" count={stats?.measurementCount} />
