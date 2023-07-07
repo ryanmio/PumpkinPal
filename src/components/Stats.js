e2a3dde288b4199971b08c7efa5d0af1dcbdf005
ryanmio <ryan@mioduski.us>
2023-07-07 17:17:43 -0400

    Update Stats.js

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -6,14 +6,14 @@ import { doc, getDoc } from "firebase/firestore";
 import { IconContext } from "react-icons";
 
 const StatCard = ({ Icon, label, count }) => (
-  <div className="flex flex-col items-center justify-center px-4 py-2 border-r border-gray-200 flex-1">
+  <div className="flex flex-col items-center justify-center p-4 border-r border-gray-200">
     <div className="flex items-center justify-center w-10 h-10 mb-3 rounded-full">
       <IconContext.Provider value={{ color: "#80876E", size: "2em" }}>
         <Icon />
       </IconContext.Provider>
     </div>
     <h6 className="text-4xl font-bold">{count !== null ? count : '--'}</h6>
-    <p className="mb-2 font-bold text-md">{label}</p>
+    <p className="mb-2 font-bold text-md text-center min-w-[6rem]">{label}</p>
   </div>
 );
 
@@ -37,8 +37,8 @@ const Stats = () => {
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center space-y-4 p-4">
-      <div className="flex w-full max-w-screen-lg bg-white shadow-md rounded-lg p-4">
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 bg-white shadow-md rounded-lg max-w-full md:max-w-3xl mx-auto w-full">
         <StatCard Icon={BsPeopleFill} label="Users" count={stats?.userCount} />
         <StatCard Icon={GiPumpkin} label="Pumpkins" count={stats?.pumpkinCount} />
         <StatCard Icon={BsClipboardData} label="Measurements" count={stats?.measurementCount} />
