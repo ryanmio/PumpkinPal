bd2cb251c7eec967cbc8c95842c9e9c7db848027
ryanmio <ryan@mioduski.us>
2023-07-07 17:29:21 -0400

    Update Stats.js

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -38,7 +38,7 @@ const Stats = () => {
 
   return (
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-      <div className="grid grid-cols-1 sm:grid-cols-3 bg-white shadow-md rounded-lg max-w-full md:max-w-3xl mx-auto w-full">
+      <div className="grid grid-cols-3 bg-white shadow-md rounded-lg max-w-full md:max-w-3xl mx-auto w-full">
         <StatCard Icon={BsPeopleFill} label="Users" count={stats?.userCount} />
         <StatCard Icon={GiPumpkin} label="Pumpkins" count={stats?.pumpkinCount} />
         <StatCard Icon={BsClipboardData} label="Measurements" count={stats?.measurementCount} />
