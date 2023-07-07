038968f58a5b3e0ef03c989ebace91ca42e60306
ryanmio <ryan@mioduski.us>
2023-07-07 17:02:21 -0400

    Update Stats.js

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -3,14 +3,17 @@ import { BsPeopleFill, BsClipboardData } from 'react-icons/bs';
 import { GiPumpkin } from 'react-icons/gi';
 import { db } from '../firebase';
 import { doc, getDoc } from "firebase/firestore";
+import { IconContext } from "react-icons";
 
 const StatCard = ({ Icon, label, count }) => (
-  <div className="flex flex-col items-center justify-center space-y-2 bg-white shadow-md p-4 rounded-lg">
-    <div className="text-4xl text-green-600">
-      <Icon />
+  <div className="flex flex-col items-center justify-center p-4 bg-white shadow-md rounded-lg text-center">
+    <div className="flex items-center justify-center w-10 h-10 mb-3 rounded-full">
+      <IconContext.Provider value={{ color: "#80876E", size: "2em" }}>
+        <Icon />
+      </IconContext.Provider>
     </div>
-    <div className="text-2xl font-bold">{count !== null ? count : '--'}</div>
-    <div>{label}</div>
+    <h6 className="text-4xl font-bold">{count !== null ? count : '--'}</h6>
+    <p className="mb-2 font-bold text-md">{label}</p>
   </div>
 );
 
@@ -34,10 +37,12 @@ const Stats = () => {
   }, []);
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 w-full px-4 sm:px-8 -mx-4 sm:-mx-8 my-8">
-      <StatCard Icon={BsPeopleFill} label="Users" count={stats?.userCount} />
-      <StatCard Icon={GiPumpkin} label="Pumpkins" count={stats?.pumpkinCount} />
-      <StatCard Icon={BsClipboardData} label="Measurements" count={stats?.measurementCount} />
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="grid gap-8 grid-cols-3">
+        <StatCard Icon={BsPeopleFill} label="Users" count={stats?.userCount} />
+        <StatCard Icon={GiPumpkin} label="Pumpkins" count={stats?.pumpkinCount} />
+        <StatCard Icon={BsClipboardData} label="Measurements" count={stats?.measurementCount} />
+      </div>
     </div>
   );
 };
