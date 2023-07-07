52f6acdb60b1e164956b2fc63ff3e79492143e18
ryanmio <ryan@mioduski.us>
2023-07-07 17:51:17 -0400

    Update Stats.js

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -37,14 +37,14 @@ const Stats = () => {
   }, []);
 
   return (
-    <div className="px-4 py-8 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-12">
-      <div className="grid grid-cols-3 bg-white shadow-md rounded-lg max-w-full md:max-w-3xl mx-auto w-full">
-        <StatCard Icon={BsPeopleFill} label="Users" count={stats?.userCount} />
-        <StatCard Icon={GiPumpkin} label="Pumpkins" count={stats?.pumpkinCount} />
-        <StatCard Icon={BsClipboardData} label="Logs" count={stats?.measurementCount} />
-      </div>
+  <div className="px-4 pt-12 pb-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-12">
+    <div className="grid grid-cols-3 bg-white shadow-md rounded-lg max-w-full md:max-w-3xl mx-auto w-full">
+      <StatCard Icon={BsPeopleFill} label="Users" count={stats?.userCount} />
+      <StatCard Icon={GiPumpkin} label="Pumpkins" count={stats?.pumpkinCount} />
+      <StatCard Icon={BsClipboardData} label="Logs" count={stats?.measurementCount} />
     </div>
-  );
+  </div>
+);
 };
 
 export default Stats;
