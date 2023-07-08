6c02aad2a830309e9b3fc67346e3183f75077e8f
ryanmio <ryan@mioduski.us>
2023-07-08 14:04:16 -0400

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -7,6 +7,7 @@ import Dropdown from './Dropdown';
 import Spinner from './Spinner';
 import PlusIcon from './icons/PlusIcon';
 import TableCellsIcon from './icons/TableCellsIcon';
+import { showDeleteConfirmation } from '../path-to-file/showDeleteConfirmation';
 
 function Dashboard() {
   const [email, setEmail] = useState('');
@@ -44,14 +45,23 @@ function Dashboard() {
   }, []);
 
   async function deletePumpkin(id) {
-    if (window.confirm("Are you sure you want to delete this pumpkin?")) {
-      setDeletionStatus('Deleting...');
-      await deleteDoc(doc(db, 'Users', auth.currentUser.uid, 'Pumpkins', id));
-      setPumpkins(pumpkins.filter(pumpkin => pumpkin.id !== id));
-      setDeletionStatus('Deleted successfully!');
-      setTimeout(() => setDeletionStatus(''), 2000);
+  showDeleteConfirmation('Are you sure you want to delete this pumpkin?', "You won't be able to undo this.", async () => {
+    try {
+      if (auth.currentUser && auth.currentUser.uid && id) {
+        setDeletionStatus('Deleting...');
+        await deleteDoc(doc(db, 'Users', auth.currentUser.uid, 'Pumpkins', id));
+        setPumpkins(pumpkins.filter(pumpkin => pumpkin.id !== id));
+        setDeletionStatus('Deleted successfully!');
+        setTimeout(() => setDeletionStatus(''), 2000);
+      } else {
+        throw new Error("Missing required parameters.");
+      }
+    } catch (error) {
+      console.error("Error deleting pumpkin: ", error);
+      toast.error("Failed to delete pumpkin. Please try again.");
     }
-  }
+  });
+}
 
   function daysSincePollination(pollinationDateStr) {
     const pollinationDate = new Date(pollinationDateStr);
