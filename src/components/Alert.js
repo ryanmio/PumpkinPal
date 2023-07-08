8bdda254acc0a97c033453e42678ba52027ff59f
ryanmio <ryan@mioduski.us>
2023-07-08 12:15:49 -0400

    Update Alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,8 +1,6 @@
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
-
-// Apply the styles for SweetAlert2
-import '../app.css';
+import '../App.css'; 
 
 const MySwal = withReactContent(Swal);
 
