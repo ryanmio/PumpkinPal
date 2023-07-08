05e18f63ec67bb444c955d5f6d5f1ea01aed8f32
ryanmio <ryan@mioduski.us>
2023-07-08 12:53:50 -0400

    Update Alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -5,37 +5,19 @@ import '../App.css';
 const MySwal = withReactContent(Swal);
 
 export const showDeleteConfirmation = (title, text, onConfirm) => {
-  MySwal.fire({
-    html: `
-      <div class="flex items-start text-left space-x-4">
-        <span class="swal2-icon swal2-warning icon-small"></span>
-        <div>
-          <h2 class="text-xl font-semibold">${title}</h2>
-          <p>${text}</p>
-        </div>
-      </div>
-    `,
+  Swal.fire({
+    title: `<div class="flex items-center">
+              <div class="icon">
+                <i class="fas fa-exclamation-circle"></i>
+              </div>
+              <div>${title}</div>
+            </div>`,
+    text: text,
+    icon: 'warning',
     showCancelButton: true,
-    confirmButtonColor: '#DF6139',
+    confirmButtonColor: '#d33',
     cancelButtonColor: '#80876E',
     confirmButtonText: 'Yes, delete it!',
-    customClass: {
-      popup: 'swal2-popup',
-      content: 'swal2-content',
-      cancelButton: 'swal2-cancel',
-      confirmButton: 'swal2-confirm',
-    },
-    buttonsStyling: false,
-    focusConfirm: false,
-    showClass: {
-      popup: 'ease-out duration-300 transform opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95',
-      backdrop: 'ease-out duration-300 opacity-0'
-    },
-    hideClass: {
-      popup: 'ease-in duration-200 transform translate-y-4 sm:translate-y-0 sm:scale-95',
-      backdrop: 'ease-in duration-200 opacity-100'
-    },
-    backdrop: 'bg-gray-500 bg-opacity-75 transition-opacity',
   }).then((result) => {
     if (result.isConfirmed) {
       onConfirm();
