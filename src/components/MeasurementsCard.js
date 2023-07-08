d86e75b96d899b88f815fc41857d86a7df8223a6
ryanmio <ryan@mioduski.us>
2023-07-08 10:29:23 -0400

    Update MeasurementsCard.js

--- src/components/MeasurementsCard.js
+++ src/components/MeasurementsCard.js
@@ -65,38 +65,38 @@ const MeasurementsCard = ({ measurements, pumpkin, pumpkinId }) => {
 };
 
   return (
-  <div className="bg-white shadow rounded-lg p-4 md:col-span-2 flex flex-col overflow-x-auto">
-    <Toaster />
-    <h3 className="text-xl font-bold mb-2">Measurements</h3>
-    <div className="flex space-x-4 justify-center">
-      <button onClick={() => navigate(`/add-measurement/${pumpkinId}`)} className="green-button inline-flex items-center justify-center px-2 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">Add Measurement</button>
-      <button onClick={exportData} className="green-button inline-flex items-center justify-center px-2 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">Export Data</button>
-    </div>
-    <div className="overflow-x-auto">
-      <table className="w-full mt-4">
-        <thead>
-          <tr>
-            <th className="whitespace-nowrap min-w-max w-[100px] table-cell">Date</th>
-            <th className="table-cell">End to End</th>
-            <th className="table-cell">Side to Side</th>
-            <th className="table-cell">Circ.</th>
-            <th className="table-cell">Units</th>
-            <th className="table-cell">OTT Weight</th>
-            <th>Edit</th>
-            <th>Delete</th>
-          </tr>
-        </thead>
-        <tbody>
-          {measurements && measurements.map(measurement => (
-            <tr key={measurement.id}>
-              <td className="whitespace-nowrap table-cell">{measurement.timestamp}</td>
-              <td className="table-cell">{measurement.endToEnd}</td>
-              <td className="table-cell">{measurement.sideToSide}</td>
-              <td className="table-cell">{measurement.circumference}</td>
-              <td className="table-cell">{measurement.measurementUnit}</td>
-              <td className="table-cell">{measurement.estimatedWeight}</td>
-              <td><button onClick={() => navigate(`/edit-measurement/${pumpkinId}/${measurement.id}`)} className="green-button inline-flex items-center justify-center px-2 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">Edit</button></td>
-              <td>
+    <div className="bg-white shadow rounded-lg p-4 md:col-span-2 flex flex-col overflow-x-auto">
+      <Toaster />
+      <h3 className="text-xl font-bold mb-2">Measurements</h3>
+      <div className="flex space-x-4 justify-center">
+        <button onClick={() => navigate(`/add-measurement/${pumpkinId}`)} className="green-button inline-flex items-center justify-center px-2 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">Add Measurement</button>
+        <button onClick={exportData} className="green-button inline-flex items-center justify-center px-2 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">Export Data</button>
+      </div>
+      <div className="overflow-x-auto">
+        <table className="w-full mt-4">
+          <thead>
+            <tr>
+              <th className="whitespace-nowrap min-w-max w-[100px] table-cell">Date</th>
+              <th className="table-cell">End to End</th>
+              <th className="table-cell">Side to Side</th>
+              <th className="table-cell">Circ.</th>
+              <th className="table-cell">Units</th>
+              <th className="table-cell">OTT Weight</th>
+              <th>Edit</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {measurements && measurements.map(measurement => (
+              <tr key={measurement.id}>
+                <td className="whitespace-nowrap table-cell">{measurement.timestamp}</td>
+                <td className="table-cell">{measurement.endToEnd}</td>
+                <td className="table-cell">{measurement.sideToSide}</td>
+                <td className="table-cell">{measurement.circumference}</td>
+                <td className="table-cell">{measurement.measurementUnit}</td>
+                <td className="table-cell">{measurement.estimatedWeight}</td>
+                <td><button onClick={() => navigate(`/edit-measurement/${pumpkinId}/${measurement.id}`)} className="green-button inline-flex items-center justify-center px-2 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">Edit</button></td>
+                <td>
                 <AlertDialog>
                   <AlertDialogTrigger asChild>
                     <button onClick={() => confirmDeleteMeasurement(measurement.id)} className="green-button inline-flex items-center justify-center px-2 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">Delete</button>
@@ -115,13 +115,13 @@ const MeasurementsCard = ({ measurements, pumpkin, pumpkinId }) => {
                   </AlertDialogContent>
                 </AlertDialog>
               </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  </div>
-);
+  );
 };
 
 export default MeasurementsCard;
