abae514c9b7542a68b263f031f64292d11f3652c
ryanmio <ryan@mioduski.us>
2023-07-08 10:26:39 -0400

    delete measurments popup modal

--- src/components/MeasurementsCard.js
+++ src/components/MeasurementsCard.js
@@ -1,28 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { doc, deleteDoc } from 'firebase/firestore';
 import { auth, db } from '../firebase';
 import { toast, Toaster } from 'react-hot-toast';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@shadcn/ui';
 
 const MeasurementsCard = ({ measurements, pumpkin, pumpkinId }) => {
   const navigate = useNavigate();
+  const [measurementToDelete, setMeasurementToDelete] = useState(null);
 
-  const deleteMeasurement = async (measurementId) => {
-    if (window.confirm("Are you sure you want to delete this measurement?")) {
-      try {
-        if (auth.currentUser && auth.currentUser.uid && pumpkinId && measurementId) {
-          const measurementPath = `Users/${auth.currentUser.uid}/Pumpkins/${pumpkinId}/Measurements/${measurementId}`;
-          await deleteDoc(doc(db, measurementPath));
-          toast.success("Measurement deleted successfully."); // Use toast here
-        } else {
-          throw new Error("Missing required parameters.");
-        }
-      } catch (error) {
-        console.error("Error deleting measurement: ", error);
-        toast.error("Failed to delete measurement. Please try again."); // And here
-      }
+  const deleteMeasurement = async () => {
+    if (!measurementToDelete) return;
+    try {
+      const measurementPath = `Users/${auth.currentUser.uid}/Pumpkins/${pumpkinId}/Measurements/${measurementToDelete}`;
+      await deleteDoc(doc(db, measurementPath));
+      toast.success("Measurement deleted successfully."); 
+      setMeasurementToDelete(null); // Reset the measurement to delete
+    } catch (error) {
+      console.error("Error deleting measurement: ", error);
+      toast.error("Failed to delete measurement. Please try again."); 
     }
-};
+  };
+    
+    const confirmDeleteMeasurement = (measurementId) => {
+    setMeasurementToDelete(measurementId);
+  };
 
   const exportData = async () => {
     toast('Exporting...', { id: 'exporting' }); // Start with an "Exporting..." toast
@@ -54,45 +65,63 @@ const MeasurementsCard = ({ measurements, pumpkin, pumpkinId }) => {
 };
 
   return (
-    <div className="bg-white shadow rounded-lg p-4 md:col-span-2 flex flex-col overflow-x-auto">
-      <Toaster />
-      <h3 className="text-xl font-bold mb-2">Measurements</h3>
-      <div className="flex space-x-4 justify-center">
-        <button onClick={() => navigate(`/add-measurement/${pumpkinId}`)} className="green-button inline-flex items-center justify-center px-2 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">Add Measurement</button>
-        <button onClick={exportData} className="green-button inline-flex items-center justify-center px-2 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">Export Data</button>
-      </div>
-      <div className="overflow-x-auto">
-        <table className="w-full mt-4">
-          <thead>
-            <tr>
-              <th className="whitespace-nowrap min-w-max w-[100px] table-cell">Date</th>
-              <th className="table-cell">End to End</th>
-              <th className="table-cell">Side to Side</th>
-              <th className="table-cell">Circ.</th>
-              <th className="table-cell">Units</th>
-              <th className="table-cell">OTT Weight</th>
-              <th>Edit</th>
-              <th>Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            {measurements && measurements.map(measurement => (
-              <tr key={measurement.id}>
-                <td className="whitespace-nowrap table-cell">{measurement.timestamp}</td>
-                <td className="table-cell">{measurement.endToEnd}</td>
-                <td className="table-cell">{measurement.sideToSide}</td>
-                <td className="table-cell">{measurement.circumference}</td>
-                <td className="table-cell">{measurement.measurementUnit}</td>
-                <td className="table-cell">{measurement.estimatedWeight}</td>
-                <td><button onClick={() => navigate(`/edit-measurement/${pumpkinId}/${measurement.id}`)} className="green-button inline-flex items-center justify-center px-2 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">Edit</button></td>
-                <td><button onClick={() => deleteMeasurement(measurement.id)} className="green-button inline-flex items-center justify-center px-2 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">Delete</button></td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+  <div className="bg-white shadow rounded-lg p-4 md:col-span-2 flex flex-col overflow-x-auto">
+    <Toaster />
+    <h3 className="text-xl font-bold mb-2">Measurements</h3>
+    <div className="flex space-x-4 justify-center">
+      <button onClick={() => navigate(`/add-measurement/${pumpkinId}`)} className="green-button inline-flex items-center justify-center px-2 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">Add Measurement</button>
+      <button onClick={exportData} className="green-button inline-flex items-center justify-center px-2 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">Export Data</button>
     </div>
-  );
-};
+    <div className="overflow-x-auto">
+      <table className="w-full mt-4">
+        <thead>
+          <tr>
+            <th className="whitespace-nowrap min-w-max w-[100px] table-cell">Date</th>
+            <th className="table-cell">End to End</th>
+            <th className="table-cell">Side to Side</th>
+            <th className="table-cell">Circ.</th>
+            <th className="table-cell">Units</th>
+            <th className="table-cell">OTT Weight</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {measurements && measurements.map(measurement => (
+            <tr key={measurement.id}>
+              <td className="whitespace-nowrap table-cell">{measurement.timestamp}</td>
+              <td className="table-cell">{measurement.endToEnd}</td>
+              <td className="table-cell">{measurement.sideToSide}</td>
+              <td className="table-cell">{measurement.circumference}</td>
+              <td className="table-cell">{measurement.measurementUnit}</td>
+              <td className="table-cell">{measurement.estimatedWeight}</td>
+              <td><button onClick={() => navigate(`/edit-measurement/${pumpkinId}/${measurement.id}`)} className="green-button inline-flex items-center justify-center px-2 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">Edit</button></td>
+              <td>
+                <AlertDialog>
+                  <AlertDialogTrigger asChild>
+                    <button onClick={() => confirmDeleteMeasurement(measurement.id)} className="green-button inline-flex items-center justify-center px-2 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">Delete</button>
+                  </AlertDialogTrigger>
+                  <AlertDialogContent>
+                    <AlertDialogHeader>
+                      <AlertDialogTitle>Delete Measurement</AlertDialogTitle>
+                      <AlertDialogDescription>
+                        Are you sure you want to delete this measurement?
+                      </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                      <AlertDialogCancel onClick={() => setMeasurementToDelete(null)}>Cancel</AlertDialogCancel>
+                      <AlertDialogAction onClick={deleteMeasurement}>Delete</AlertDialogAction>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialog>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  </div>
+);
+
 
 export default MeasurementsCard;
