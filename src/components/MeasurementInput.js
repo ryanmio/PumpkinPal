793ea624266639e3e843eed15404d02ac7f595df
ryanmio <ryan@mioduski.us>
2023-07-09 11:54:35 -0400

    Update MeasurementInput.js

--- src/components/MeasurementInput.js
+++ src/components/MeasurementInput.js
@@ -5,7 +5,7 @@ function MeasurementInput({ id, value, onChange, placeholder, min, max }) {
     <div className="relative">
       <input 
         id={id}
-        type="number"
+        type="text"
         value={value}
         placeholder=" "
         onChange={onChange}
@@ -13,7 +13,6 @@ function MeasurementInput({ id, value, onChange, placeholder, min, max }) {
         max={max}
         step="0.5" // This line allows 0.5 increments
         required
-        inputMode="decimal"
         className={`input text-center text-2xl w-full p-2 border-2 border-gray-300 rounded ${value ? "filled" : ""}`}
       />
       <label htmlFor={id} className="label-float absolute left-0 top-0">{placeholder}</label>
