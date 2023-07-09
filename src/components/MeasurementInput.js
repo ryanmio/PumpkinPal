c8ee4cc5fb345031ea7e04b79deaf7e7eba8ddff
ryanmio <ryan@mioduski.us>
2023-07-09 11:35:47 -0400

    Update MeasurementInput.js

--- src/components/MeasurementInput.js
+++ src/components/MeasurementInput.js
@@ -13,7 +13,6 @@ function MeasurementInput({ id, value, onChange, placeholder, min, max }) {
         max={max}
         step="0.5" // This line allows 0.5 increments
         required
-        pattern="\d*(\.\d+)?"
         inputMode="decimal"
         className={`input text-center text-2xl w-full p-2 border-2 border-gray-300 rounded ${value ? "filled" : ""}`}
       />
