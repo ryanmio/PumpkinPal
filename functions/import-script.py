81bdeea59c13e88541e57c8f557cc545345ae6fe
ryanmio <ryan@mioduski.us>
2023-07-16 12:36:13 -0400

    Update import-script.py

--- functions/import-script.py
+++ functions/import-script.py
@@ -20,19 +20,29 @@ df['Weight (lbs)'] = df['Weight (lbs)'].str.replace(',', '').astype(float)
 error_flag = False
 
 # Define function to upload documents to Firestore
-def upload_to_firestore(collection, doc_id, data):
-    global error_flag
-    try:
-        db.collection(collection).document(doc_id).set(data)
-        print(f"Uploaded document {doc_id} to collection {collection}")
-    except Exception as e:
-        print(f"Error uploading document {doc_id} to collection {collection}: {e}")
-        error_flag = True
+def upload_to_firestore(collection, documents):
+    # Split documents into batches of 500
+    batches = [documents[i:i + 500] for i in range(0, len(documents), 500)]
+
+    for batch_documents in batches:
+        batch = db.batch()
+        for doc_id, data in batch_documents:
+            doc_ref = db.collection(collection).document(doc_id)
+            batch.set(doc_ref, data)
+        batch.commit()
+
+    print(f"Uploaded {len(documents)} documents to collection {collection}")
+
+# Create empty lists to hold documents
+grower_documents = []
+contest_documents = []
+pumpkin_documents = []
 
 # Process each row in the dataframe (only the first 10 rows for testing)
 for index, row in df.head(100).iterrows():  # Change this to df.iterrows() to process all rows
     # Create or update grower document
     grower_data = {
+        "id": row["Processed Name"],
         "firstName": row["First Name"],
         "lastName": row["Last Name"],
         "city": row["City"],
@@ -40,20 +50,22 @@ for index, row in df.head(100).iterrows():  # Change this to df.iterrows() to pr
         "country": row["Country"],
         "timestamp": datetime.now()
     }
-    upload_to_firestore("Stats_Growers", row["Processed Name"], grower_data)
+    grower_documents.append((row["Processed Name"], grower_data))
 
     # Create or update contest document
     contest_id = f'{row["GPC Site"]}_{row["Year"]}'
     contest_data = {
+        "id": contest_id,
         "name": row["GPC Site"],
         "year": row["Year"],
         "timestamp": datetime.now()
     }
-    upload_to_firestore("Stats_Contests", contest_id, contest_data)
+    contest_documents.append((contest_id, contest_data))
 
     # Create pumpkin document
     pumpkin_id = f'{row["Weight (lbs)"]} {row["Last Name"] if pd.notnull(row["Last Name"]) else row["Processed Name"]}'.strip()  # Use weight and last name as document ID
     pumpkin_data = {
+        "id": pumpkin_id,
         "weight": row["Weight (lbs)"],
         "place": row["Place"],
         "seed": row["Seed (Mother)"],
@@ -65,15 +77,23 @@ for index, row in df.head(100).iterrows():  # Change this to df.iterrows() to pr
         "year": row["Year"], 
         "timestamp": datetime.now()
     }
-    # Check that all necessary fields are present
-    if pd.notnull(pumpkin_id) and pd.notnull(pumpkin_data["weight"]) and pd.notnull(pumpkin_data["grower"]) and pd.notnull(pumpkin_data["contest"]):
-        upload_to_firestore("Stats_Pumpkins", pumpkin_id, pumpkin_data)
-    else:
-        print(f"Error: Missing or invalid data for pumpkin {pumpkin_id}.")
-        error_flag = True
+    pumpkin_documents.append((pumpkin_id, pumpkin_data))
 
-# Check if there were any errors
-if error_flag:
-    print("There were errors during the data upload. Please review the error messages above.")
-else:
-    print("Data upload completed successfully.")
+# Upload documents to Firestore
+try:
+    upload_to_firestore("Stats_Growers", grower_documents)
+    print("Grower data upload completed successfully.")
+except Exception as e:
+    print("There were errors during the grower data upload. Please review the error messages above.")
+
+try:
+    upload_to_firestore("Stats_Contests", contest_documents)
+    print("Contest data upload completed successfully.")
+except Exception as e:
+    print("There were errors during the contest data upload. Please review the error messages above.")
+
+try:
+    upload_to_firestore("Stats_Pumpkins", pumpkin_documents)
+    print("Pumpkin data upload completed successfully.")
+except Exception as e:
+    print("There were errors during the pumpkin data upload. Please review the error messages above.")
