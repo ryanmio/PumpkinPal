bebd4e754ac81de92467cf66503b77d4c80c525b
ryanmio <ryan@mioduski.us>
2023-07-20 09:26:17 -0400

    Update GrowerContext.js

--- src/contexts/GrowerContext.js
+++ src/contexts/GrowerContext.js
@@ -1,17 +1,42 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import { toast } from 'react-hot-toast';
+import { db } from '../firebase';
 
-const GrowerContext = createContext();
+export const GrowerContext = createContext();
 
-const GrowerProvider = ({ children }) => {
+export const GrowerProvider = ({ children }) => {
+  const [growerName, setGrowerName] = useState(null);
   const [growerData, setGrowerData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
+  useEffect(() => {
+    const fetchGrowerData = async () => {
+      setLoading(true);
+      try {
+        const growerDoc = await db.collection('Stats_Growers').doc(growerName).get();
+        if (!growerDoc.exists) {
+          throw new Error(`No grower found with the name "${growerName}".`);
+        }
+        const pumpkinDocs = await db.collection('Stats_Pumpkins').where('grower', '==', growerName).get();
+        const growerData = { ...growerDoc.data(), pumpkins: pumpkinDocs.docs.map(doc => doc.data()) };
+        setGrowerData(growerData);
+        setLoading(false);
+      } catch (error) {
+        setError(error.message);
+        toast.error(error.message);
+        setLoading(false);
+      }
+    };
+
+    if (growerName) {
+      fetchGrowerData();
+    }
+  }, [growerName]);
+
   return (
-    <GrowerContext.Provider value={{ growerData, setGrowerData, loading, setLoading, error, setError }}>
+    <GrowerContext.Provider value={{ growerName, setGrowerName, growerData, error }}>
       {children}
     </GrowerContext.Provider>
   );
 };
-
-export { GrowerProvider, GrowerContext };
