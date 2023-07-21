745b0118a8d9419449428d4acfb6dfbb05859bb1
ryanmio <ryan@mioduski.us>
2023-07-21 17:42:10 -0400

    Create fetchGrowerData.js

---
+++ src/utilities/fetchGrowerData.js
@@ -0,0 +1,20 @@
+import { firestore } from './firebase'; // replace with your Firebase import
+
+const fetchGrowerData = async (growerId) => {
+  try {
+    const growerRef = firestore.collection('Stats_Growers').doc(growerId);
+    const doc = await growerRef.get();
+    
+    if (doc.exists) {
+      return doc.data(); // assuming doc.data() is the grower's data
+    } else {
+      console.log('No such document!');
+      return null;
+    }
+  } catch (error) {
+    console.error('Error fetching grower data:', error);
+    return null;
+  }
+};
+
+export default fetchGrowerData;
