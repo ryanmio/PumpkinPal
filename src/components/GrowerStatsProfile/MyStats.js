e336aadec686fe7c5b5b17cbaea9eb1e3a302038
ryanmio <ryan@mioduski.us>
2023-07-21 17:36:08 -0400

    Update MyStats.js

--- src/components/GrowerStatsProfile/MyStats.js
+++ src/components/GrowerStatsProfile/MyStats.js
@@ -1,22 +1,31 @@
-import { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { firestore } from '../firebase'; // import Firestore
+import { UserContext } from '../../contexts/UserContext';
 import { GrowerContext } from '../../contexts/GrowerContext';
-import { useAuth } from '../../contexts/AuthContext'; // import your AuthContext
 import Header from './Header';
 import SummarySection from './SummarySection';
 import TableSection from './TableSection';
 
 const MyStats = () => {
-  const { currentUser } = useAuth(); // get the current user from the AuthContext
+  const { user } = useContext(UserContext); // get the current user from the UserContext
   const { growerData, loading, error } = useContext(GrowerContext);
   const [growerId, setGrowerId] = useState(null);
+  const [editing, setEditing] = useState(false);
 
   useEffect(() => {
-    if (currentUser) {
-      // fetch the grower ID from the user's profile and set it to the state
-      // replace 'getGrowerId' with the actual function to fetch the grower ID
-      getGrowerId(currentUser.uid).then(setGrowerId);
+    if (user) {
+      // fetch the grower ID from the user's profile in Firestore
+      firestore.collection('Users').doc(user.uid).get().then(doc => {
+        if (doc.exists) {
+          setGrowerId(doc.data().growerId);
+        } else {
+          console.log('No such document!');
+        }
+      }).catch(error => {
+        console.log('Error getting document:', error);
+      });
     }
-  }, [currentUser]);
+  }, [user]);
 
   useEffect(() => {
     if (growerId) {
@@ -25,6 +34,22 @@ const MyStats = () => {
       fetchGrowerData(growerId);
     }
   }, [growerId]);
+
+  const handleEdit = () => {
+    setEditing(true);
+  };
+
+  const handleSave = (newGrowerId) => {
+    // update the grower ID in Firestore
+    firestore.collection('Users').doc(user.uid).update({
+      growerId: newGrowerId
+    }).then(() => {
+      setGrowerId(newGrowerId);
+      setEditing(false);
+    }).catch(error => {
+      console.error('Error updating document:', error);
+    });
+  };
 
   if (loading) {
     return <div>Loading...</div>;
@@ -43,6 +68,17 @@ const MyStats = () => {
       <Header data={growerData} />
       <SummarySection data={growerData} />
       <TableSection data={growerData.pumpkins} />
+      {editing ? (
+        <div>
+          <input type="text" defaultValue={growerId} onChange={e => setGrowerId(e.target.value)} />
+          <button onClick={() => handleSave(growerId)}>Save</button>
+        </div>
+      ) : (
+        <div>
+          <p>Grower ID: {growerId}</p>
+          <button onClick={handleEdit}>Edit</button>
+        </div>
+      )}
     </div>
   );
 };
