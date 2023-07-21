3c6281156bd237f9d36974cc286aa1a39ce77619
ryanmio <ryan@mioduski.us>
2023-07-21 18:01:34 -0400

    Update Search.js

--- src/components/GrowerStatsProfile/Search.js
+++ src/components/GrowerStatsProfile/Search.js
@@ -1,24 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { db } from '../../firebase'; // assuming you have a firebase.js file where you initialize firebase
 import { useNavigate } from 'react-router-dom';
 
-const GrowerSearch = () => {
-  const [growerName, setGrowerName] = useState('');
+const Search = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    getSuggestions();
+  }, []);
+
+  const getSuggestions = async () => {
+    const contests = await db.collection('Stats_Contests').get();
+    const contestNames = contests.docs.map(doc => ({ type: 'contest', name: doc.data().name }));
+
+    const pumpkins = await db.collection('Stats_Pumpkins').get();
+    const pumpkinIds = pumpkins.docs.map(doc => ({ type: 'pumpkin', id: doc.data().id }));
+
+    setSuggestions([...contestNames, ...pumpkinIds]);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    navigate(`/grower/${encodeURIComponent(growerName.trim())}`);
+    const matchedSuggestion = suggestions.find(suggestion => suggestion.name === searchTerm || suggestion.id === searchTerm);
+    if (matchedSuggestion) {
+      if (matchedSuggestion.type === 'contest') {
+        navigate(`/contest/${encodeURIComponent(searchTerm.trim())}`);
+      } else {
+        navigate(`/pumpkin/${encodeURIComponent(searchTerm.trim())}`);
+      }
+    } else {
+      // handle case where no matching suggestion is found
+    }
   };
 
   return (
     <div>
-      <h1>Search for a Grower</h1>
+      <h1>Search</h1>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
-          value={growerName}
-          onChange={(e) => setGrowerName(e.target.value)}
-          placeholder="Enter grower name"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Enter contest name or pumpkin ID"
         />
         <button type="submit">Search</button>
       </form>
@@ -26,4 +51,4 @@ const GrowerSearch = () => {
   );
 };
 
-export default GrowerSearch;
+export default Search;
