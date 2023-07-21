70f49a1a71de235e7bcdb3fa3726347caea48bba
ryanmio <ryan@mioduski.us>
2023-07-21 17:42:09 -0400

    Create fetchPumpkins.js

---
+++ src/utilities/fetchPumpkins.js
@@ -0,0 +1,20 @@
+import { firestore } from './firebase';
+
+const fetchPumpkins = async (growerId) => {
+  try {
+    const pumpkinsRef = firestore.collection('Stats_Pumpkins');
+    const snapshot = await pumpkinsRef.where('growerId', '==', growerId).get();
+    
+    const pumpkins = [];
+    snapshot.forEach(doc => {
+      pumpkins.push(doc.data()); // assuming doc.data() is the pumpkin's data
+    });
+
+    return pumpkins;
+  } catch (error) {
+    console.error('Error fetching pumpkins:', error);
+    return [];
+  }
+};
+
+export default fetchPumpkins;
