fe606bb79d4f3e76336017dffaf09c6fe1941020
ryanmio <ryan@mioduski.us>
2023-07-21 08:33:53 -0400

    Update GrowerContext.js

--- src/contexts/GrowerContext.js
+++ src/contexts/GrowerContext.js
@@ -11,26 +11,27 @@ export const GrowerProvider = ({ children }) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const fetchGrowerData = async () => {
+        const fetchGrowerData = async () => {
       setLoading(true);
+      console.log('Fetching data for grower:', growerName); // Log the growerName
       try {
-        console.log('Fetching grower data...');
         const growerDoc = await db.collection('Stats_Growers').doc(growerName).get();
+        console.log('Grower data from Firestore:', growerDoc.data()); // Log the growerDoc
         if (!growerDoc.exists) {
           throw new Error(`No grower found with the name "${growerName}".`);
         }
         const pumpkinDocs = await db.collection('Stats_Pumpkins').where('grower', '==', growerName).get();
+        console.log('Pumpkin data from Firestore:', pumpkinDocs.docs.map(doc => doc.data())); // Log the pumpkinDocs
         const growerData = { ...growerDoc.data(), pumpkins: pumpkinDocs.docs.map(doc => doc.data()) };
         setGrowerData(growerData);
         setLoading(false);
-        console.log('Finished fetching grower data.');
       } catch (error) {
-        console.error('Error fetching grower data:', error);
         setError(error.message);
         toast.error(error.message);
         setLoading(false);
       }
     };
+
 
     if (growerName) {
       fetchGrowerData();
