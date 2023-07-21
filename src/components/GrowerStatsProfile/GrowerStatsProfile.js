e876dd6f7078915d436b57c23039e86e751534b9
ryanmio <ryan@mioduski.us>
2023-07-21 08:49:44 -0400

    Update GrowerStatsProfile.js

--- src/components/GrowerStatsProfile/GrowerStatsProfile.js
+++ src/components/GrowerStatsProfile/GrowerStatsProfile.js
@@ -1,15 +1,19 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { GrowerContext } from '../../contexts/GrowerContext';
+import { useParams } from 'react-router-dom';
 import Header from './Header';
 import SummarySection from './SummarySection';
 import TableSection from './TableSection';
 
 const GrowerStatsProfile = () => {
-  const { growerData, loading } = useContext(GrowerContext);
+  const { growerName, setGrowerName, growerData, loading } = useContext(GrowerContext);
+  const { growerName: growerNameFromUrl } = useParams(); // get growerName from the URL
 
-  console.log('Rendering GrowerStatsProfile component...');
-  console.log('growerData:', growerData);
-  console.log('loading:', loading);
+  useEffect(() => {
+    if (growerName !== growerNameFromUrl) {
+      setGrowerName(growerNameFromUrl); // update growerName in the context if it's different from the one in the URL
+    }
+  }, [growerName, growerNameFromUrl, setGrowerName]);
 
   if (loading || !growerData) {
     return <div>Loading...</div>;
