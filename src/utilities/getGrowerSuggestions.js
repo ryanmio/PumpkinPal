25b241bbb7d04978a20b400ec9e62a3459533159
ryanmio <ryan@mioduski.us>
2023-07-21 16:09:03 -0400

    Update getGrowerSuggestions.js

--- src/utilities/getGrowerSuggestions.js
+++ src/utilities/getGrowerSuggestions.js
@@ -1,10 +1,20 @@
 import { firestore } from './firebase'; // replace with your Firebase import
 import debounce from 'lodash.debounce';
 
+// Function to convert a string to title case
+function toTitleCase(str) {
+  return str.replace(/\w\S*/g, function(txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+}
+
 const getGrowerSuggestions = debounce(async (inputValue, callback) => {
   try {
+    // Convert inputValue to title case
+    const titleCaseInput = toTitleCase(inputValue);
+
     const growerRef = firestore.collection('Stats_Growers');
-    const snapshot = await growerRef.where('lastName', '>=', inputValue).where('lastName', '<=', inputValue + '\uf8ff').get();
+    const snapshot = await growerRef.where('lastName', '>=', titleCaseInput).where('lastName', '<=', titleCaseInput + '\uf8ff').get();
     
     const growers = [];
     snapshot.forEach(doc => {
