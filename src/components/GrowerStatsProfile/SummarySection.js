89babae1029197f91362673083dea76554e6d067
ryanmio <ryan@mioduski.us>
2023-07-22 15:12:13 -0400

    Update SummarySection.js

--- src/components/GrowerStatsProfile/SummarySection.js
+++ src/components/GrowerStatsProfile/SummarySection.js
@@ -3,7 +3,7 @@ const SummarySection = ({ data }) => (
     <h2>Summary</h2>
     <p>Country: {data.country}</p>
     <p>State: {data.state}</p>
-    <p>Total Entries: {data.pumpkins.length}</p>
+    <p>Total Entries: {data.pumpkins ? data.pumpkins.length : 0}</p>
     {/* More stats here */}
   </div>
 );
