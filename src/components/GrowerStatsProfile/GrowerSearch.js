8cc14ae8e21e804a11de3e9565be354052961607
ryanmio <ryan@mioduski.us>
2023-07-22 12:12:49 -0400

    Update GrowerSearch.js

--- src/components/GrowerStatsProfile/GrowerSearch.js
+++ src/components/GrowerStatsProfile/GrowerSearch.js
@@ -1,27 +1,67 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import getGrowerSuggestions from '../../utilities/getGrowerSuggestions';
+import fetchPumpkins from '../../utilities/fetchPumpkins';
+import { doc, updateDoc } from 'firebase/firestore';
+import { db } from '../../firebase';
 
-const GrowerSearch = () => {
+const GrowerSearch = ({ user, setGrowerId }) => { // accept user as a prop
   const [growerName, setGrowerName] = useState('');
-  const navigate = useNavigate();
+  const [suggestions, setSuggestions] = useState([]);
+  const [selectedGrower, setSelectedGrower] = useState(null);
+  const [pumpkinPreview, setPumpkinPreview] = useState([]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    navigate(`/grower/${encodeURIComponent(growerName.trim())}`);
+  useEffect(() => {
+    if (growerName) {
+      getGrowerSuggestions(growerName, setSuggestions);
+    }
+  }, [growerName]);
+
+  useEffect(() => {
+    if (selectedGrower) {
+      fetchPumpkins(selectedGrower.id).then(setPumpkinPreview);
+    }
+  }, [selectedGrower]);
+
+  const handleSelectGrower = (grower) => {
+    setSelectedGrower(grower);
+  };
+
+  const handleConfirm = () => {
+    updateDoc(doc(db, 'Users', user.uid), {
+      growerId: selectedGrower.id
+    }).then(() => {
+      setGrowerId(selectedGrower.id);
+    }).catch(error => {
+      console.error('Error updating document:', error);
+    });
   };
 
   return (
     <div>
       <h1>Search for a Grower</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={growerName}
-          onChange={(e) => setGrowerName(e.target.value)}
-          placeholder="Enter grower name"
-        />
-        <button type="submit">Search</button>
-      </form>
+      <input
+        type="text"
+        value={growerName}
+        onChange={(e) => setGrowerName(e.target.value)}
+        placeholder="Enter grower name"
+      />
+      {suggestions.map(suggestion => (
+        <div key={suggestion.id} onClick={() => handleSelectGrower(suggestion)}>
+          {suggestion.name}
+        </div>
+      ))}
+      {selectedGrower && (
+        <div>
+          <h2>Selected Grower: {selectedGrower.name}</h2>
+          <h3>Pumpkin Preview:</h3>
+          {pumpkinPreview.map(pumpkin => (
+            <div key={pumpkin.id}>
+              {pumpkin.name}
+            </div>
+          ))}
+          <button onClick={handleConfirm}>Confirm</button>
+        </div>
+      )}
     </div>
   );
 };
