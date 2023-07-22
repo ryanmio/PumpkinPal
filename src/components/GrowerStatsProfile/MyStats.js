73a07042a32805ffd5548748217e35f067eccba2
ryanmio <ryan@mioduski.us>
2023-07-22 19:18:08 -0400

    Update MyStats.js

--- src/components/GrowerStatsProfile/MyStats.js
+++ src/components/GrowerStatsProfile/MyStats.js
@@ -1,6 +1,4 @@
 import React, { useContext, useState } from 'react';
-import { db } from '../../firebase';
-import { doc, updateDoc } from 'firebase/firestore';
 import { UserContext } from '../../contexts/UserContext';
 import Header from './Header';
 import SummarySection from './SummarySection';
@@ -11,31 +9,18 @@ import useGrowerData from '../../utilities/useGrowerDataHook';
 
 const MyStats = () => {
   const { user, growerId, setGrowerId } = useContext(UserContext);
-    console.log('Rendering MyStats with user:', user);
-  const [editingGrowerId, setEditingGrowerId] = useState(false);
-  const { growerData, pumpkins, loading } = useGrowerData(user?.uid);
+  const { growerData, pumpkins, loading } = useGrowerData(user ? user.uid : null);
 
   const handleEdit = () => {
-    setEditingGrowerId(true);
-  };
-
-  const handleSave = (newGrowerId) => {
-    updateDoc(doc(db, 'Users', user.uid), {
-      growerId: newGrowerId
-    }).then(() => {
-      setEditingGrowerId(false);
-      setGrowerId(newGrowerId);
-    }).catch(error => {
-      console.error('Error updating document:', error);
-    });
+    setGrowerId(null); // Clear the current growerId when starting to edit
   };
 
   if (loading) {
     return <Spinner />;
   }
 
-  if (editingGrowerId || !growerId) {
-    return <GrowerSearch user={user} setGrowerId={handleSave} />;
+  if (!growerId) {
+    return <GrowerSearch user={user} setGrowerId={setGrowerId} />;
   }
 
   return (
