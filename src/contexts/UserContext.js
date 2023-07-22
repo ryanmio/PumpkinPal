691d691a32ddd36db2472c6e8d1659019646d0a1
ryanmio <ryan@mioduski.us>
2023-07-22 18:05:41 -0400

    Update UserContext.js

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,16 +1,32 @@
 import React, { createContext, useState, useEffect } from 'react';
-import { auth } from '../firebase';
+import { auth, db } from '../firebase';
+import { doc, onSnapshot } from 'firebase/firestore';
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [growerId, setGrowerId] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      setUser(user);
-      setLoading(false);
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        onSnapshot(doc(db, 'Users', userAuth.uid), (docSnap) => {
+          if (docSnap.exists) {
+            setUser(userAuth);
+            setGrowerId(docSnap.data().growerId);
+          } else {
+            setUser(userAuth);
+            setGrowerId(null);
+          }
+          setLoading(false);
+        });
+      } else {
+        setUser(null);
+        setGrowerId(null);
+        setLoading(false);
+      }
     });
 
     // Cleanup subscription on unmount
@@ -22,7 +38,7 @@ export const UserProvider = ({ children }) => {
   }
 
   return (
-    <UserContext.Provider value={{user, loading}}>
+    <UserContext.Provider value={{user, growerId, loading}}>
       {children}
     </UserContext.Provider>
   );
