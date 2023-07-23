c88066e244f0a44abb941600c645b9af320a9440
ryanmio <ryan@mioduski.us>
2023-07-23 16:41:06 -0400

    Update SummarySection.js

--- src/components/GrowerStatsProfile/SummarySection.js
+++ src/components/GrowerStatsProfile/SummarySection.js
@@ -1,7 +1,7 @@
 const SummarySection = ({ data }) => (
   <div>
     <h2>Summary</h2>
-    <p>Total Entries: {data.totalEntries}</p>
+    <p>Total Entries: {data.NumberOfEntries}</p>
     <p>Global Ranking: {data.globalRanking}</p>
     <p>Country Ranking: {data.countryRanking}</p>
     <p>State Ranking: {data.stateRanking}</p>
