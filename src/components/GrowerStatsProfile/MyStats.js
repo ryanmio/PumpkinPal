7b1f8046a44f7939cac4e6088a249984509350ac
ryanmio <ryan@mioduski.us>
2023-07-22 21:13:14 -0400

    Update MyStats.js

--- src/components/GrowerStatsProfile/MyStats.js
+++ src/components/GrowerStatsProfile/MyStats.js
@@ -20,18 +20,14 @@ const MyStats = () => {
   };
 
   const handleSave = (newGrowerId) => {
-    updateDoc(doc(db, 'Users', user.uid), {
-      growerId: newGrowerId
-    }).then(() => {
-      onSnapshot(doc(db, 'Users', user.uid), (docSnap) => {
-        if (docSnap.exists) {
-          setGrowerId(docSnap.data().growerId);
-        }
-      });
-    }).catch(error => {
-      console.error('Error updating document:', error);
-    });
-  };
+  updateDoc(doc(db, 'Users', user.uid), {
+    growerId: newGrowerId
+  }).then(() => {
+    setGrowerId(newGrowerId);
+  }).catch(error => {
+    console.error('Error updating document:', error);
+  });
+};
 
   if (loading) {
     return <Spinner />;
