0b05c351ef8ed04f7b9e4d4f19c7e7b8ea81071b
ryanmio <ryan@mioduski.us>
2023-07-23 16:37:00 -0400

    Update SummarySection.js

--- src/components/GrowerStatsProfile/SummarySection.js
+++ src/components/GrowerStatsProfile/SummarySection.js
@@ -1,13 +1,11 @@
 const SummarySection = ({ data }) => (
   <div>
     <h2>Summary</h2>
-    <p>Country: {data.country}</p>
-    <p>State: {data.state}</p>
     <p>Total Entries: {data.totalEntries}</p>
     <p>Global Ranking: {data.globalRanking}</p>
     <p>Country Ranking: {data.countryRanking}</p>
     <p>State Ranking: {data.stateRanking}</p>
-    <p>Max Pumpkin Weight: {data.maxWeight}</p>
+    <p>Max Pumpkin Weight: {data.LifetimeMaxWeight}</p>
     <p>Best Rank: {data.bestRank}</p>
   </div>
 );
