49bc68dbb7c63b92a2fb71bc61dbeecaf07fd815
ryanmio <ryan@mioduski.us>
2023-07-23 11:12:36 -0400

    Update fetchPumpkins.js

--- src/utilities/fetchPumpkins.js
+++ src/utilities/fetchPumpkins.js
@@ -1,5 +1,6 @@
 import { db } from '../firebase';
 import { collection, query, where, getDocs } from 'firebase/firestore';
+import { toast } from 'react-hot-toast';
 
 const fetchPumpkins = (growerId) => {
   return new Promise(async (resolve, reject) => {
@@ -20,7 +21,7 @@ const fetchPumpkins = (growerId) => {
 
       resolve(pumpkins);
     } catch (error) {
-      console.error('Error fetching pumpkins:', error);
+      toast.error('Error fetching pumpkins: ' + error.message);
       reject(error);
     }
   });
