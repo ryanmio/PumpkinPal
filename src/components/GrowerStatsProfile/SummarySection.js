0dd6cbe3210b7304b5611fe580e5f74498b9a1fe
ryanmio <ryan@mioduski.us>
2023-07-24 19:14:50 -0400

    Update SummarySection.js

--- src/components/GrowerStatsProfile/SummarySection.js
+++ src/components/GrowerStatsProfile/SummarySection.js
@@ -1,17 +1,13 @@
-const SummarySection = ({ data }) => {
-  const globalRanking = data.globalRanking.replace("Global: ", "");
-
-  return (
-    <div className="bg-white shadow rounded-lg p-4 md:col-span-2 flex flex-col">
-      <h2 className="text-xl font-bold mb-2">Lifetime Stats</h2>
-      <p>Total Entries: {data.NumberOfEntries}</p>
-      <p>Grower Rank: {globalRanking}</p>
-      <p>Country Ranking: {data.countryRanking}</p>
-      <p>State Ranking: {data.stateRanking}</p>
-      <p>Max Pumpkin Weight: {data.LifetimeMaxWeight}</p>
-      <p>Best Rank: {data.bestRank}</p>
-    </div>
-  );
-};
+const SummarySection = ({ data }) => (
+  <div className="bg-white shadow rounded-lg p-4 md:col-span-2 flex flex-col">
+    <h2 className="text-xl font-bold mb-2">Lifetime Stats</h2>
+    <p>Total Entries: {data.NumberOfEntries}</p>
+    <p>Global Ranking: {data.globalRanking}</p>
+    <p>Country Ranking: {data.countryRanking}</p>
+    <p>State Ranking: {data.stateRanking}</p>
+    <p>Max Pumpkin Weight: {data.LifetimeMaxWeight}</p>
+    <p>Best Rank: {data.bestRank}</p>
+  </div>
+);
 
 export default SummarySection;
