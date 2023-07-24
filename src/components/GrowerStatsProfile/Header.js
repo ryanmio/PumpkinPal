4a099b417803b6082de95b7f83fc1c8e70f52ea9
ryanmio <ryan@mioduski.us>
2023-07-24 19:07:52 -0400

    Update Header.js

--- src/components/GrowerStatsProfile/Header.js
+++ src/components/GrowerStatsProfile/Header.js
@@ -1,8 +1,12 @@
-const Header = ({ data }) => (
-  <div className="bg-white shadow rounded-lg p-4">
-    <h1 className="text-2xl font-bold mb-2">{data.firstName} {data.lastName}</h1>
-    <h2 className="text-xl font-medium">Global Rank: {data.globalRanking}</h2>
-  </div>
-);
+const Header = ({ data }) => {
+  const strippedRanking = data.globalRanking.replace('Global: ', '');
+
+  return (
+    <div className="bg-white shadow rounded-lg p-4">
+      <h1 className="text-2xl font-bold mb-2">{data.firstName} {data.lastName}</h1>
+      <h2 className="text-xl font-medium">Grower Rank: {strippedRanking}</h2>
+    </div>
+  );
+};
 
 export default Header;
