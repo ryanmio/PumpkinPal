2b3be1407e0c5d8640c0d0b3864200d1c8fcacb2
ryanmio <ryan@mioduski.us>
2023-07-24 08:38:46 -0400

    Update MyStats.js

--- src/components/GrowerStatsProfile/MyStats.js
+++ src/components/GrowerStatsProfile/MyStats.js
@@ -54,7 +54,7 @@ const MyStats = () => {
         <Header data={growerData} />
         <SummarySection data={growerData} />
         <TableSection data={pumpkins} columns={pumpkinColumns} />
-        <div className="bg-white shadow rounded-lg p-4 flex flex-col">
+        <div className="bg-white shadow rounded-lg p-4 mb-4 flex flex-col">
           <p><b>Grower ID:</b> {growerId}</p>
           <button onClick={handleEdit} className="green-button inline-flex items-center justify-center px-2 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 self-center">Change</button>
         </div>
