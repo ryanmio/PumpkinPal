aed34d8ee9b1510b5ad623613fcd544d9d9e0b5f
ryanmio <ryan@mioduski.us>
2023-07-24 18:59:56 -0400

    Update Header.js

--- src/components/GrowerStatsProfile/Header.js
+++ src/components/GrowerStatsProfile/Header.js
@@ -1,7 +1,7 @@
 const Header = ({ data }) => (
   <div className="bg-white shadow rounded-lg p-4">
     <h1 className="text-2xl font-bold mb-2">{data.firstName} {data.lastName}</h1>
-    <h2 className="text-xl font-medium">Global Rank: {data.bestRank}</h2>
+    <h2 className="text-xl font-medium">Global Rank: {data.globalRanking}</h2>
   </div>
 );
 
