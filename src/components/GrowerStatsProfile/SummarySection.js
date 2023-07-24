524ed3241325322e540957ec9e22f16a974b7e1d
ryanmio <ryan@mioduski.us>
2023-07-24 19:01:11 -0400

    Update SummarySection.js

--- src/components/GrowerStatsProfile/SummarySection.js
+++ src/components/GrowerStatsProfile/SummarySection.js
@@ -1,6 +1,6 @@
 const SummarySection = ({ data }) => (
   <div className="bg-white shadow rounded-lg p-4 md:col-span-2 flex flex-col">
-    <h2 className="text-xl font-bold mb-2">Summary</h2>
+    <h2 className="text-xl font-bold mb-2">Lifetime Stats</h2>
     <p>Total Entries: {data.NumberOfEntries}</p>
     <p>Global Ranking: {data.globalRanking}</p>
     <p>Country Ranking: {data.countryRanking}</p>
