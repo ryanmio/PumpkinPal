d9f8fe43ecfe2193ed50cef7e9d355bb02907945
ryanmio <ryan@mioduski.us>
2023-07-29 12:44:53 -0400

    Update PumpkinDetails.js

--- src/components/GrowerStatsProfile/PumpkinDetails.js
+++ src/components/GrowerStatsProfile/PumpkinDetails.js
@@ -19,20 +19,24 @@ const PumpkinDetailsCard = ({ data }) => (
 );
 
 const PumpkinRankingsCard = ({ data }) => (
-  <div className="bg-white shadow rounded-lg p-4 mb-4 grid grid-cols-3 gap-4 text-center">
-    <div className="col-span-3 text-lg font-bold mb-2">Ranking Matrix</div> {/* Heading */}
-    <div></div> {/* Empty cell */}
-    <div><b>{data.year}</b></div> {/* Year data */}
-    <div><b>All Time</b></div> {/* All Time data */}
-    <div><b>Global</b></div>
-    <div>#{data.yearGlobalRank}</div> {/* '#' added to ranking */}
-    <div>#{data.lifetimeGlobalRank}</div> {/* '#' added to ranking */}
-    <div><b>{data.country}</b></div> {/* Replaced "Country" with actual country data */}
-    <div>#{data.yearlyCountryRank}</div> {/* '#' added to ranking */}
-    <div>#{data.lifetimeCountryRank}</div> {/* '#' added to ranking */}
-    <div><b>{data.state}</b></div> {/* Replaced "State" with actual state data */}
-    <div>#{data.yearlyStateRank}</div> {/* '#' added to ranking */}
-    <div>#{data.lifetimeStateRank}</div> {/* '#' added to ranking */}
+  <div className="bg-white shadow rounded-lg p-4 mb-4 flex justify-center">
+    <div className="max-w-lg w-full"> {/* This div sets a max width and wraps the card content */}
+      <div className="grid grid-cols-3 gap-4 text-center">
+        <div className="col-span-3 text-lg font-bold mb-2">Ranking Matrix</div> {/* Heading */}
+        <div></div> {/* Empty cell */}
+        <div><b>{data.year}</b></div> {/* Year data */}
+        <div><b>All Time</b></div> {/* All Time data */}
+        <div><b>Global</b></div>
+        <div>#{data.yearGlobalRank}</div> {/* '#' added to ranking */}
+        <div>#{data.lifetimeGlobalRank}</div> {/* '#' added to ranking */}
+        <div><b>{data.country}</b></div> {/* Replaced "Country" with actual country data */}
+        <div>#{data.yearlyCountryRank}</div> {/* '#' added to ranking */}
+        <div>#{data.lifetimeCountryRank}</div> {/* '#' added to ranking */}
+        <div><b>{data.state}</b></div> {/* Replaced "State" with actual state data */}
+        <div>#{data.yearlyStateRank}</div> {/* '#' added to ranking */}
+        <div>#{data.lifetimeStateRank}</div> {/* '#' added to ranking */}
+      </div>
+    </div>
   </div>
 );
 
