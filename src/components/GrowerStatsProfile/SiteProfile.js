04f10eb5353fc224e9afb496d48e6a0698fd4ce8
ryanmio <ryan@mioduski.us>
2023-07-29 15:16:14 -0400

    Update SiteProfile.js

--- src/components/GrowerStatsProfile/SiteProfile.js
+++ src/components/GrowerStatsProfile/SiteProfile.js
@@ -1,24 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link, useNavigate } from 'react-router-dom';
-import { collection, getDocs, query, where } from 'firebase/firestore';
+import { doc, getDoc } from 'firebase/firestore';
 import { db } from '../../firebase';
 import Spinner from '../Spinner';
 
-// Component for displaying contest details
-const ContestDetailsCard = ({ data }) => (
+// Component for displaying site details
+const SiteDetailsCard = ({ data }) => (
   <div className="bg-white shadow rounded-lg p-4 mb-4">
-    <h1>{data.name}</h1> {/* changed from data.id to data.name */}
-    <p><b>Year:</b> {data.year}</p>
-    <p><b>Record Weight:</b> {data.recordWeight}</p>
-    <p><b>Year Popularity:</b> {data.YearPopularity}</p>
-    <p><b>Lifetime Popularity:</b> {data.LifetimePopularity}</p>
+    <h1>{data.id}</h1> 
+    <p><b>Site Record:</b> {data['Site Record']}</p>
+    <p><b>Total Entries:</b> {data['Total Entries']}</p>
+    <p><b>Popularity by Year:</b> {JSON.stringify(data['Popularity by Year'])}</p>
+    <p><b>Max Weight by Year:</b> {JSON.stringify(data['Max Weight by Year'])}</p>
   </div>
 );
 
 const SiteProfile = () => {
   const { id } = useParams();
   const siteName = id.replace(/_/g, ' ');
-  const [contestData, setContestData] = useState(null);
+  const [siteData, setSiteData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const navigate = useNavigate();
@@ -29,16 +29,12 @@ const SiteProfile = () => {
       try {
         setLoading(true);
         setError(null);
-        const contestsQuery = query(collection(db, 'Stats_Contests'), where('name', '==', siteName));
-        const querySnapshot = await getDocs(contestsQuery);
-        if (!querySnapshot.empty) {
-          let data = {};
-          querySnapshot.forEach((doc) => {
-            data = {...data, ...doc.data()};
-          });
-          setContestData(data);
+        const docRef = doc(db, 'Stats_Sites', siteName);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          setSiteData({ id: siteName, ...docSnap.data() });
         } else {
-          setError(`No contests found for site: ${siteName}`);
+          setError(`No site found with name: ${siteName}`);
         }
       } catch (err) {
         setError(err.message);
@@ -67,7 +63,7 @@ const SiteProfile = () => {
         <div className="text-left">
           <Link to="#" onClick={() => navigate(-1)} className="text-gray-700 hover:text-gray-900 transition duration-150 ease-in-out">Back</Link>
         </div>
-        <ContestDetailsCard data={contestData} />
+        <SiteDetailsCard data={siteData} />
       </div>
     </div>
   );
