7cf97b8d9894d5eb225386b58a8972efe62935e1
ryanmio <ryan@mioduski.us>
2023-07-29 08:48:42 -0400

    Update TableSection.js

--- src/components/GrowerStatsProfile/TableSection.js
+++ src/components/GrowerStatsProfile/TableSection.js
@@ -14,38 +14,40 @@ const TableSection = ({ data, columns }) => {
   }, useSortBy);
 
   return (
-    <div className="bg-white shadow rounded-lg p-4 flex flex-col overflow-x-auto">
-      <h2 className="text-xl font-bold mb-2">Weigh-Off History</h2>
-      <table {...getTableProps()} className="w-full">
-        <thead>
-          {headerGroups.map(headerGroup => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column => (
-                <th {...column.getHeaderProps(column.getSortByToggleProps())} className={`whitespace-nowrap min-w-max table-cell ${column.id === 'contestName' ? 'w-[300px]' : column.id === 'year' ? 'w-[75px]' : 'w-[100px]'}`}>
-                  {column.render('Header')}
-                  <span>
-                    {column.isSorted ? (column.isSortedDesc ? ' ▾' : ' ▴') : ''}
-                  </span>
-                </th>
+  <div className="bg-white shadow rounded-lg p-4 flex flex-col overflow-x-auto">
+    <h2 className="text-xl font-bold mb-2">Weigh-Off History</h2>
+    <table {...getTableProps()} className="w-full">
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps(column.getSortByToggleProps())} className={`whitespace-nowrap min-w-max table-cell ${column.id === 'contestName' ? 'w-[300px]' : column.id === 'year' ? 'w-[75px]' : 'w-[100px]'}`}>
+                {column.render('Header')}
+                <span>
+                  {column.isSorted ? (column.isSortedDesc ? ' ▾' : ' ▴') : ''}
+                </span>
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map(row => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => (
+                <td {...cell.getCellProps()} className={`table-cell overflow-hidden whitespace-nowrap text-overflow-ellipsis ${cell.column.id === 'contestName' ? 'w-[200px]' : cell.column.id === 'year' ? 'w-[75px]' : 'w-[100px]'}`}>
+                  {cell.render('Cell')}
+                </td>
               ))}
             </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map(row => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map(cell => (
-                  <td {...cell.getCellProps()} className={`table-cell ${cell.column.id === 'contestName' ? 'w-[200px]' : cell.column.id === 'year' ? 'w-[75px]' : 'w-[100px]'}`}>{cell.render('Cell')}</td>
-                ))}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
+          );
+        })}
+      </tbody>
+    </table>
+  </div>
+);
 };
 
 export default TableSection;
