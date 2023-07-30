e558264f53fd26c826413e62790c89728bee082f
ryanmio <ryan@mioduski.us>
2023-07-30 13:17:46 -0400

    Update SiteProfile.js

--- src/components/GrowerStatsProfile/SiteProfile.js
+++ src/components/GrowerStatsProfile/SiteProfile.js
@@ -12,18 +12,18 @@ const SiteDetailsCard = ({ data, popularityData, weightData }) => {
   return (
   <div className="bg-white shadow rounded-lg p-4 mb-4">
     <h1>{data.id}</h1>
-    <div className="grid grid-cols-3 gap-4 mb-6">
-      <div style={{backgroundColor: '#FFD699'}} className="p-6 rounded text-center">
-        <h2 className="text-lg font-bold mb-2">Site Record (lbs)</h2>
-        <p className="text-2xl">{data['Site Record']}</p>
+        <div className="flex justify-between mb-6 border-b border-gray-200 pb-4">
+      <div className="text-center">
+        <h2 className="text-lg mb-2">Site Record (lbs)</h2>
+        <p className="text-2xl font-light">{data['Site Record']}</p>
       </div>
-      <div style={{backgroundColor: '#FFD699'}} className="p-6 rounded text-center">
-        <h2 className="text-lg font-bold mb-2">Total Entries</h2>
-        <p className="text-2xl">{data['Total Entries']}</p>
+      <div className="text-center">
+        <h2 className="text-lg mb-2">Total Entries</h2>
+        <p className="text-2xl font-light">{data['Total Entries']}</p>
       </div>
-      <div style={{backgroundColor: '#FFD699'}} className="p-6 rounded text-center">
-        <h2 className="text-lg font-bold mb-2">Unique Years</h2>
-        <p className="text-2xl">{uniqueYears}</p>
+      <div className="text-center">
+        <h2 className="text-lg mb-2">Unique Years</h2>
+        <p className="text-2xl font-light">{uniqueYears}</p>
       </div>
     </div>
     <p><b>Entries by Year:</b></p>
