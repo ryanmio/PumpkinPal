e9fa9e522069d674beef5802e70437fc254c36f1
ryanmio <ryan@mioduski.us>
2023-08-03 18:41:07 -0400

    Update GraphCard.js

--- src/components/GraphCard.js
+++ src/components/GraphCard.js
@@ -35,7 +35,7 @@ const GraphCard = ({ measurements, pumpkinName }) => {
 
 
   return (
-    <div className="bg-white shadow rounded-lg p-4 md:col-span-2 flex flex-col overflow-x-auto mb-12">
+    <div className="bg-white shadow rounded-lg p-4 md:col-span-2 flex flex-col overflow-x-auto">
       <h3 className="text-xl font-bold mb-2">{pumpkinName} Weight Trend</h3>
       <Line data={chartData} options={options} />
     </div>
