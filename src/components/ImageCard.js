e7a2e4061039d74ae7a0e05cff7caaea3d708992
ryanmio <ryan@mioduski.us>
2023-08-03 19:09:57 -0400

    Update ImageCard.js

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,46 +4,36 @@ import { toast } from 'react-hot-toast';
 import PlusIcon from './icons/PlusIcon';
 
 const ImageCard = ({ pumpkinId }) => {
-  const [images, setImages] = useState([]);
   const [previewUrls, setPreviewUrls] = useState([]);
 
-  const handleImageChange = (e) => {
+  const handleImageChange = async (e) => {
     const file = e.target.files[0];
     if (file) {
-      setImages([...images, file]);
       // Preview the selected image
       const reader = new FileReader();
       reader.onload = (e) => {
         setPreviewUrls([...previewUrls, e.target.result]);
       };
       reader.readAsDataURL(file);
-    }
-  };
 
-  const handleUpload = async () => {
-    try {
-      const downloadUrls = [];
-      for (const image of images) {
-        // Define the storage path
-        const storagePath = `path/to/storage/${pumpkinId}/${image.name}`;
-        const storageRef = storage.ref(storagePath);
-        const snapshot = await storageRef.put(image);
-        const downloadUrl = await snapshot.ref.getDownloadURL();
-        downloadUrls.push(downloadUrl);
+      // Define the storage path
+      const storagePath = `path/to/storage/${pumpkinId}/${file.name}`;
+      const storageRef = storage.ref(storagePath);
+      try {
+        await storageRef.put(file);
+        toast.success('Image uploaded successfully.');
         // Placeholder: Save the URL to Firestore or other relevant location
+      } catch (error) {
+        console.error('Error uploading image:', error);
+        toast.error('Failed to upload image. Please try again.');
       }
-      toast.success('Images uploaded successfully.');
-      // You can set the download URLs to the state or handle them as needed
-    } catch (error) {
-      console.error('Error uploading images:', error);
-      toast.error('Failed to upload images. Please try again.');
     }
   };
 
   return (
     <div className="bg-white shadow rounded-lg p-4 md:col-span-2 flex flex-col overflow-x-auto mb-12">
       <h3 className="text-xl font-bold mb-2">Image Gallery</h3>
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-2 gap-4">
         {previewUrls.map((url, index) => (
           <img key={index} src={url} alt="Preview" className="w-full h-64 object-cover" />
         ))}
@@ -58,12 +48,6 @@ const ImageCard = ({ pumpkinId }) => {
           <PlusIcon className="h-8 w-8 text-gray-400" />
         </label>
       </div>
-      <button
-        onClick={handleUpload}
-        className="green-button inline-flex items-center justify-center px-2 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 mt-4"
-      >
-        Upload
-      </button>
     </div>
   );
 };
