cc50da74b8095cd93eb3142db4a7121ac835ed62
ryanmio <ryan@mioduski.us>
2023-08-04 12:11:09 -0400

    Update ImageCard.js

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,25 +1,34 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { storage, db } from '../firebase';
 import { toast } from 'react-hot-toast';
 import PlusIcon from './icons/PlusIcon';
 import { UserContext } from '../contexts/UserContext';
-import { updateDoc, arrayUnion, collection, doc } from 'firebase/firestore';
+import { updateDoc, arrayUnion, collection, doc, getDoc } from 'firebase/firestore';
 import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 
 const ImageCard = ({ pumpkinId }) => {
   const [images, setImages] = useState([]);
-  const [previewUrls, setPreviewUrls] = useState([]);
   const { user } = useContext(UserContext);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      const usersCollection = collection(db, 'Users');
+      const userDoc = doc(usersCollection, user.uid);
+      const pumpkinsCollection = collection(userDoc, 'Pumpkins');
+      const pumpkinRef = doc(pumpkinsCollection, pumpkinId);
+
+      const pumpkinDoc = await getDoc(pumpkinRef);
+      if (pumpkinDoc.exists()) {
+        setImages(pumpkinDoc.data().images || []);
+      }
+    };
+
+    fetchImages();
+  }, [user.uid, pumpkinId]);
 
   const handleImageChange = async (e) => {
     const file = e.target.files[0];
     if (file) {
-      setImages([...images, file]);
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        setPreviewUrls([...previewUrls, e.target.result]);
-      };
-      reader.readAsDataURL(file);
       await handleUpload(file);
     }
   };
@@ -49,6 +58,7 @@ const ImageCard = ({ pumpkinId }) => {
 
             // Update the pumpkin document with the new download URL
             updateDoc(pumpkinRef, { images: arrayUnion(downloadURL) });
+            setImages(prevImages => [...prevImages, downloadURL]);
             toast.success('Image uploaded successfully.');
           });
         }
@@ -63,7 +73,7 @@ const ImageCard = ({ pumpkinId }) => {
     <div className="bg-white shadow rounded-lg p-4 md:col-span-2 flex flex-col overflow-x-auto mb-12">
       <h3 className="text-xl font-bold mb-4">Image Gallery</h3>
       <div className="grid grid-cols-2 gap-4">
-        {previewUrls.map((url, index) => (
+        {images.map((url, index) => (
           <img key={index} src={url} alt="Preview" className="w-full h-64 object-cover" />
         ))}
         <label className="w-full h-64 flex justify-center items-center border-2 border-dashed border-gray-400 rounded cursor-pointer hover:bg-gray-100">
