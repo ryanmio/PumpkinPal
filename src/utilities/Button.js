6af0dd90512010052add0ec237facd351d6b7abc
ryanmio <ryan@mioduski.us>
2023-08-04 13:15:20 -0400

    Update Button.js

--- src/utilities/Button.js
+++ src/utilities/Button.js
@@ -4,7 +4,7 @@ function Button({ onClick, children, disabled, type = 'button', extraClasses = '
       type={type} 
       onClick={onClick} 
       disabled={disabled}
-      className={`inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-green-button hover:bg-green-button-hover focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-button ${extraClasses}`}
+      className={`green-button inline-flex items-center justify-center px-2 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 self-end ${extraClasses}`}
     >
       {children}
     </button>
