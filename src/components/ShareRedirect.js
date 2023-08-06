10c09462413ea3a70e946bb7b97a697652100fe9
ryanmio <ryan@mioduski.us>
2023-08-06 18:07:43 -0400

    Update ShareRedirect.js

--- src/components/ShareRedirect.js
+++ src/components/ShareRedirect.js
@@ -1,29 +1,13 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { db } from '../firebase';
-import { doc, getDoc } from 'firebase/firestore';
 
 const ShareRedirect = () => {
   const { imageId } = useParams(); // Using imageId to match the document ID
 
   useEffect(() => {
-    const fetchSharedImageUrl = async () => {
-      try {
-        const sharedImageRef = doc(db, 'SharedImages', imageId);
-        const sharedImageDoc = await getDoc(sharedImageRef);
-
-        if (sharedImageDoc.exists()) {
-          const sharedImageUrl = sharedImageDoc.data().image; // Getting the image URL
-          window.location.replace(sharedImageUrl);
-        } else {
-          console.error('Shared image not found');
-        }
-      } catch (error) {
-        console.error('Error fetching shared image:', error);
-      }
-    };
-
-    fetchSharedImageUrl();
+    // Redirecting to the cloud function URL with the image ID
+    const cloudFunctionUrl = `https://us-central1-pumpkinpal-b60be.cloudfunctions.net/renderSharedImage/image/${imageId}`;
+    window.location.replace(cloudFunctionUrl);
   }, [imageId]);
 
   return <div>Redirecting...</div>;
