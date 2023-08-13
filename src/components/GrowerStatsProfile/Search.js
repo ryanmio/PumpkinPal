8557958b42eb8576edc0b7e324b42bb8cef8a880
ryanmio <ryan@mioduski.us>
2023-08-13 18:51:27 -0400

    Update Search.js

--- src/components/GrowerStatsProfile/Search.js
+++ src/components/GrowerStatsProfile/Search.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import algoliasearch from 'algoliasearch/lite';
-import { InstantSearch, SearchBox, Hits } from 'react-instantsearch';
+import { InstantSearch, connectSearchBox, Hits } from 'react-instantsearch';
 import { useNavigate } from 'react-router-dom';
 
 const searchClient = algoliasearch('SPV52PLJT9', '46d4c9707d1655c9a75d6949e02615a0');
@@ -21,15 +21,28 @@ const Hit = ({ hit }) => {
   );
 };
 
+const CustomSearchBox = ({ currentRefinement, refine }) => {
+  return (
+    <input
+      type="text"
+      value={currentRefinement}
+      onChange={e => refine(e.target.value)}
+      placeholder="Search..."
+    />
+  );
+};
+
+const ConnectedSearchBox = connectSearchBox(CustomSearchBox);
+
 const Search = () => {
-  const [query, setQuery] = useState(''); // Track the query state
+  const [query, setQuery] = useState('');
 
   return (
     <div>
       <h1>Search</h1>
       <InstantSearch searchClient={searchClient} indexName="Sites">
-        <SearchBox onSearchChange={(e) => setQuery(e.query)} /> {/* Update the query state on change */}
-        {query && <Hits hitComponent={Hit} />} {/* Render Hits only when query is present */}
+        <ConnectedSearchBox onSearchStateChange={({ query }) => setQuery(query)} />
+        {query && <Hits hitComponent={Hit} />}
       </InstantSearch>
     </div>
   );
