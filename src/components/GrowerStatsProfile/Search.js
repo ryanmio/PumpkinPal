83ce6a6790f36102ecfbbf726068748250245317
ryanmio <ryan@mioduski.us>
2023-08-13 13:28:29 -0400

    search with algolia

--- src/components/GrowerStatsProfile/Search.js
+++ src/components/GrowerStatsProfile/Search.js
@@ -1,53 +1,44 @@
-import React, { useState, useEffect } from 'react';
-import { db } from '../../firebase'; // assuming you have a firebase.js file where you initialize firebase
-import { collection, getDocs } from 'firebase/firestore'; // import collection, getDocs
+import React from 'react';
+import algoliasearch from 'algoliasearch/lite';
+import {
+  InstantSearch,
+  SearchBox,
+  Hits,
+  Highlight,
+} from 'react-instantsearch';
 import { useNavigate } from 'react-router-dom';
 
-const Search = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [suggestions, setSuggestions] = useState([]);
+const searchClient = algoliasearch(
+  'SPV52PLJT9', // Replace with your Algolia Application Id
+  '46d4c9707d1655c9a75d6949e02615a0' // Replace with your Algolia Search-Only API Key
+);
+
+const Hit = ({ hit }) => {
   const navigate = useNavigate();
 
-  useEffect(() => {
-    getSuggestions();
-  }, []);
-
-  const getSuggestions = async () => {
-    const contests = await getDocs(collection(db, 'Stats_Contests'));
-    const contestNames = contests.docs.map(doc => ({ type: 'contest', name: doc.data().name }));
-
-    const pumpkins = await getDocs(collection(db, 'Stats_Pumpkins'));
-    const pumpkinIds = pumpkins.docs.map(doc => ({ type: 'pumpkin', id: doc.data().id }));
-
-    setSuggestions([...contestNames, ...pumpkinIds]);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const matchedSuggestion = suggestions.find(suggestion => suggestion.name === searchTerm || suggestion.id === searchTerm);
-    if (matchedSuggestion) {
-      if (matchedSuggestion.type === 'contest') {
-        navigate(`/contest/${encodeURIComponent(searchTerm.trim())}`);
-      } else {
-        navigate(`/pumpkin/${encodeURIComponent(searchTerm.trim())}`);
-      }
+  const handleHitClick = () => {
+    if (hit.type === 'contest') {
+      navigate(`/contest/${encodeURIComponent(hit.name)}`);
     } else {
-      // handle case where no matching suggestion is found
+      navigate(`/pumpkin/${encodeURIComponent(hit.id)}`);
     }
   };
 
   return (
+    <div onClick={handleHitClick}>
+      <Highlight attribute="name" hit={hit} />
+    </div>
+  );
+};
+
+const Search = () => {
+  return (
     <div>
       <h1>Search</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          placeholder="Enter contest name or pumpkin ID"
-        />
-        <button type="submit">Search</button>
-      </form>
+      <InstantSearch searchClient={searchClient} indexName="Sites">
+        <SearchBox />
+        <Hits hitComponent={Hit} />
+      </InstantSearch>
     </div>
   );
 };
