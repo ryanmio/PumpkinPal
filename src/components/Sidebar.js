b37f0b4b7ca3cc30f2d262740c5b7812603a2993
ryanmio <ryan@mioduski.us>
2023-08-20 19:38:57 -0400

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,41 +3,25 @@ import { Link } from 'react-router-dom';
 
 function Sidebar() {
   return (
-    <div className="sidebar">
-      <div className="sidebar-brand">
-        <Link to="/">Your Logo</Link>
+    <aside id="default-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+      <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+        <ul className="space-y-2 font-medium">
+          <li>
+            <Link to="/dashboard" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+              <i className="bi bi-speedometer2 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></i>
+              <span className="ml-3">Dashboard</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/add-pumpkin" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+              <i className="bi bi-plus-circle w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></i>
+              <span className="ml-3">Add Pumpkin</span>
+            </Link>
+          </li>
+          {/* Add other links here */}
+        </ul>
       </div>
-      <div className="sidebar-menu">
-        <Link to="/dashboard" className="sidebar-link">
-          <i className="bi bi-speedometer2"></i>
-          Dashboard
-        </Link>
-        <Link to="/add-pumpkin" className="sidebar-link">
-          <i className="bi bi-plus-circle"></i>
-          Add Pumpkin
-        </Link>
-        <Link to="/add-measurement/:id" className="sidebar-link">
-          <i className="bi bi-ruler"></i>
-          Add Measurement
-        </Link>
-        <Link to="/" className="sidebar-link">
-          <i className="bi bi-house"></i>
-          Homepage
-        </Link>
-        <Link to="/user-profile" className="sidebar-link">
-          <i className="bi bi-person"></i>
-          User Profile
-        </Link>
-        <Link to="/search" className="sidebar-link">
-          <i className="bi bi-search"></i>
-          Search
-        </Link>
-        <Link to="/my-stats" className="sidebar-link">
-          <i className="bi bi-graph-up"></i>
-          My Stats
-        </Link>
-      </div>
-    </div>
+    </aside>
   );
 }
 
