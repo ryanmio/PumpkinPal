accb26ecccc813d60a23f5d2faaf256b78196fd9
ryanmio <ryan@mioduski.us>
2023-08-21 19:14:28 -0400

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,7 +9,7 @@ function Sidebar({ isOpen, toggleSidebar }) {
 
   return (
     <>
-      <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+      <nav className="fixed top-0 z-50 w-full bg-#80876E border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700"> {/* Updated color */}
         <div className="flex items-center justify-between px-3 py-3 lg:px-5 lg:pl-3">
           <div className="flex items-center">
             <button
@@ -23,8 +23,10 @@ function Sidebar({ isOpen, toggleSidebar }) {
                 <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
               </svg>
             </button>
+          </div>
+          <div className="flex items-center justify-center w-full"> {/* Centered logo */}
             <Link to={currentUser ? "/dashboard" : "/"}>
-              <img src="/logowide.png" alt="Logo" className="App-logo ml-3" />
+              <img src="/logowide.png" alt="Logo" className="App-logo" />
             </Link>
           </div>
           <div className="flex items-center">
