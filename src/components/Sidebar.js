52df5bea2b9570448da25dda213d08b6c64005af
ryanmio <ryan@mioduski.us>
2023-08-21 19:07:26 -0400

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,20 +1,17 @@
-import React, { useContext } from 'react'; // import useContext
+import React, { useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { UserContext } from '../contexts/UserContext'; // import UserContext
+import { UserContext } from '../contexts/UserContext';
 import Logout from './Logout';
 
 function Sidebar({ isOpen, toggleSidebar }) {
-  const currentUser = useContext(UserContext); // get the current user from UserContext
+  const currentUser = useContext(UserContext);
   const navigate = useNavigate();
 
   return (
     <>
       <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
         <div className="flex items-center justify-between px-3 py-3 lg:px-5 lg:pl-3">
-          <div className="flex items-center justify-start">
-            <Link to={currentUser ? "/dashboard" : "/"}>
-              <img src="/logowide.png" alt="Logo" className="App-logo" style={{ paddingLeft: currentUser ? "0px" : "20px" }} />
-            </Link>
+          <div className="flex items-center">
             <button
               onClick={toggleSidebar}
               aria-controls="logo-sidebar"
@@ -26,6 +23,11 @@ function Sidebar({ isOpen, toggleSidebar }) {
                 <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
               </svg>
             </button>
+            <Link to={currentUser ? "/dashboard" : "/"}>
+              <img src="/logowide.png" alt="Logo" className="App-logo ml-3" />
+            </Link>
+          </div>
+          <div className="flex items-center">
             {currentUser ? (
               <Logout className="logout-button" />
             ) : (
