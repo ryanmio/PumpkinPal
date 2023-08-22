8568cca143faa8951293b75d6d2c567c45664fc6
ryanmio <ryan@mioduski.us>
2023-08-21 20:28:59 -0400

    menu icon

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { UserContext } from '../contexts/UserContext';
 import Logout from './Logout';
+import MenuIcon from '../Icons/MenuIcon';
 
 function Sidebar({ isOpen, toggleSidebar }) {
   const currentUser = useContext(UserContext);
@@ -19,9 +20,7 @@ function Sidebar({ isOpen, toggleSidebar }) {
               className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             >
               <span className="sr-only">Open sidebar</span>
-              <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-              </svg>
+              <MenuIcon color="white" />
             </button>
           </div>
           <div className="flex items-center justify-center w-full"> {/* Centered logo */}
