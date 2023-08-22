83a54fecdd8f3fc639c1fe7b235f869ff7e92089
ryanmio <ryan@mioduski.us>
2023-08-22 18:12:39 -0400

    icon tweaks

--- src/components/icons/DashboardIcon.js
+++ src/components/icons/DashboardIcon.js
@@ -1,17 +1,14 @@
 import React from 'react';
 
-function DashboardIcon() {
+function DashboardIcon({ color = '#80876E' }) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" x="0px" y="0px" className="w-5 h-5 text-gray-500 icon-hover">
-      <title>dashboard</title>
-      <g>
-        <rect x="17" y="6" width="9" height="12" rx="1"/>
-        <rect x="6" y="15" width="9" height="12" rx="1"/>
-        <rect x="6" y="6" width="9" height="8" rx="1"/>
-        <rect x="17" y="19" width="9" height="8" rx="1"/>
-      </g>
+    <svg style={{ fill: color }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" x="0px" y="0px">
+      <path d="m11,6h-3c-2.21,0-4,1.79-4,4v12c0,2.21,1.79,4,4,4h3c2.21,0,4-1.79,4-4v-12c0-2.21-1.79-4-4-4Zm2,16c0,1.1-.9,2-2,2h-3c-1.1,0-2-.9-2-2v-12c0-1.1.9-2,2-2h3c1.1,0,2,.9,2,2v12Z"/>
+      <path d="m25,6h-5c-1.65,0-3,1.35-3,3v3c0,1.65,1.35,3,3,3h5c1.65,0,3-1.35,3-3v-3c0-1.65-1.35-3-3-3Zm1,6c0,.55-.45,1-1,1h-5c-.55,0-1-.45-1-1v-3c0-.55.45-1,1-1h5c.55,0,1,.45,1,1v3Z"/>
+      <path d="m25,17h-5c-1.65,0-3,1.35-3,3v3c0,1.65,1.35,3,3,3h5c1.65,0,3-1.35,3-3v-3c0-1.65-1.35-3-3-3Zm1,6c0,.55-.45,1-1,1h-5c-.55,0-1-.45-1-1v-3c0-.55.45-1,1-1h5c.55,0,1,.45,1,1v3Z"/>
     </svg>
   );
 }
+
 
 export default DashboardIcon;
