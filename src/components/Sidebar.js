2afb99ae829df62f0089845fda05e63b513e0af1
ryanmio <ryan@mioduski.us>
2023-08-22 18:06:31 -0400

    hover states on sidebar icons

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -44,14 +44,14 @@ function Sidebar({ isOpen, toggleSidebar }) {
         <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
           <ul className="space-y-2 font-medium">
             <li>
-              <Link to="/dashboard" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+              <Link to="/dashboard" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group link">
                 <DashboardIcon />
                 <span className="ml-3">Dashboard</span>
               </Link>
             </li>
             <li>
-              <Link to="/add-pumpkin" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                {/* Add icon here */}
+              <Link to="/add-pumpkin" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group link">
+                <DashboardIcon />
                 <span className="ml-3">Add Pumpkin</span>
               </Link>
             </li>
--- src/components/icons/DashboardIcon.js
+++ src/components/icons/DashboardIcon.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function DashboardIcon() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" x="0px" y="0px" className="w-5 h-5 text-gray-500">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" x="0px" y="0px" className="w-5 h-5 text-gray-500 icon-hover">
       <title>dashboard</title>
       <g>
         <rect x="17" y="6" width="9" height="12" rx="1"/>
