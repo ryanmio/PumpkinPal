41366c5336addf8d9730df4f46f87a84864951b1
ryanmio <ryan@mioduski.us>
2023-08-23 12:37:20 -0400

    Update Logout.js

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -18,7 +18,7 @@ function Logout() {
   return (
     <button
       onClick={handleLogout}
-      className="relative px-5 py-3 overflow-hidden font-medium text-gray-600 bg-gray-100 border border-gray-100 rounded-lg shadow-inner group"
+      className="relative overflow-hidden font-medium text-gray-600 bg-gray-100 border border-gray-100 rounded-lg shadow-inner group"
     >
       <span className="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease"></span>
       <span className="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-full ease"></span>
