7ca077f0b610aea71086e485cc543ff7283ef74f
ryanmio <ryan@mioduski.us>
2023-08-23 18:24:12 -0400

    Update SignInButton.js

--- src/components/SignInButton.js
+++ src/components/SignInButton.js
@@ -11,8 +11,8 @@ function SignInButton() {
   return (
     <button
       onClick={handleSignIn}
-      className="relative px-2 py-1 text-sm font-medium text-gray-600 bg-gray-100 border border-gray-100 rounded-lg shadow-inner group focus:outline-none whitespace-nowrap overflow-hidden"
-      style={{ '--hover-bg-color': '#4A4F40', minWidth: '80px' }}
+      className="relative px-2 py-1 text-sm font-medium text-white bg-gray-100 border border-gray-100 rounded-lg shadow-inner group focus:outline-none whitespace-nowrap overflow-hidden" // Updated text color to white
+      style={{ '--hover-bg-color': '#4A4F40', minWidth: '70px' }}
     >
       <span className="absolute top-0 left-0 w-full h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-0 ease"></span>
       <span className="absolute bottom-0 right-0 w-full h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-0 ease"></span>
