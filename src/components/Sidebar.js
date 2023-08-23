f47b007bb5c13f5bd1ee9bc7582bae65123d95dc
ryanmio <ryan@mioduski.us>
2023-08-23 19:32:04 -0400

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -17,7 +17,7 @@ function Sidebar({ isOpen, toggleSidebar }) {
 
   return (
     <>
-      <nav className="fixed top-0 z-50 w-full bg-#80876E border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+      <nav className="fixed top-0 z-50 w-full border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700" style={{ backgroundColor: '#80876E' }}>
         <div className="flex items-center justify-between px-3 py-3 lg:px-5 lg:pl-3">
           <div className="flex items-center">
           {currentUser && (
