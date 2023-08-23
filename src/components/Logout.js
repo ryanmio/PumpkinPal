25c7c337f6c5cc72912f0ffe8f0d5ff89952f703
ryanmio <ryan@mioduski.us>
2023-08-23 17:21:48 -0400

    sign out button

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -19,13 +19,14 @@ function Logout() {
     <button
       onClick={handleLogout}
       className="relative px-2 py-1 text-sm font-medium text-gray-600 bg-gray-100 border border-gray-100 rounded-lg shadow-inner group focus:outline-none overflow-hidden" // Added overflow-hidden
+      style={{ '--hover-bg-color': '#4A4F40' }} // Added custom property for hover background color
     >
       <span className="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease rounded-lg"></span> {/* Added border radius */}
       <span className="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-full ease rounded-lg"></span> {/* Added border radius */}
-      <span className="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease rounded-lg"></span> {/* Added border radius */}
-      <span className="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease rounded-lg"></span> {/* Added border radius */}
-      <span className="absolute inset-0 w-full h-full duration-300 delay-300 bg-gray-900 opacity-0 group-hover:opacity-100 rounded-lg"></span> {/* Added border radius */}
-      <span className="relative transition-colors duration-300 delay-200 group-hover:text-white ease">Logout</span>
+      <span className="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease rounded-lg" style={{ backgroundColor: 'var(--hover-bg-color)' }}></span> {/* Added custom property */}
+      <span className="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease rounded-lg" style={{ backgroundColor: 'var(--hover-bg-color)' }}></span> {/* Added custom property */}
+      <span className="absolute inset-0 w-full h-full duration-300 delay-300 bg-gray-900 opacity-0 group-hover:opacity-100 rounded-lg" style={{ backgroundColor: 'var(--hover-bg-color)' }}></span> {/* Added custom property */}
+      <span className="relative transition-colors duration-300 delay-200 group-hover:text-white ease">Sign Out</span>
     </button>
   );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -48,7 +48,7 @@ function Sidebar({ isOpen, toggleSidebar }) {
     <span className="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
     <span className="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
     <span className="absolute inset-0 w-full h-full duration-300 delay-300 bg-gray-900 opacity-0 group-hover:opacity-100"></span>
-    <span className="relative transition-colors duration-300 delay-200 group-hover:text-white ease">Login</span>
+    <span className="relative transition-colors duration-300 delay-200 group-hover:text-white ease">Sign In</span>
   </button>
 )}
           </div>
