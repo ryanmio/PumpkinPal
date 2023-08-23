c004c5eb59a36f32aa2dd667923f6fd6df442079
ryanmio <ryan@mioduski.us>
2023-08-23 17:47:34 -0400

    Update Logout.js

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -18,8 +18,8 @@ function Logout() {
   return (
     <button
       onClick={handleLogout}
-      className="relative px-1 py-1 text-sm font-medium text-gray-600 bg-gray-100 border border-gray-100 rounded-lg shadow-inner group focus:outline-none whitespace-nowrap"
-      style={{ '--hover-bg-color': '#4A4F40', minWidth: '80px' }} // Adjusted minimum width
+      className="relative px-2 py-1 text-sm font-medium text-gray-600 bg-gray-100 border border-gray-100 rounded-lg shadow-inner group focus:outline-none whitespace-nowrap"
+      style={{ '--hover-bg-color': '#4A4F40', minWidth: '40px' }} // Adjusted minimum width
     >
       <span className="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease rounded-lg"></span>
       <span className="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-full ease rounded-lg"></span>
