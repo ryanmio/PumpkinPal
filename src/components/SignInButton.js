28e13a3641b6083469cb0347a0e5751faeefd276
ryanmio <ryan@mioduski.us>
2023-08-23 18:27:59 -0400

    Update SignInButton.js

--- src/components/SignInButton.js
+++ src/components/SignInButton.js
@@ -11,7 +11,7 @@ function SignInButton() {
   return (
     <button
       onClick={handleSignIn}
-      className="relative px-2 py-1 text-sm font-medium text-white bg-gray-100 border border-gray-100 rounded-lg shadow-inner group focus:outline-none whitespace-nowrap overflow-hidden" // Updated text color to white
+      className="relative px-2 py-1 text-sm font-medium text-white bg-gray-100 border border-gray-100 rounded-lg shadow-inner group focus:outline-none whitespace-nowrap overflow-hidden"
       style={{ '--hover-bg-color': '#4A4F40', minWidth: '70px' }}
     >
       <span className="absolute top-0 left-0 w-full h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-0 ease"></span>
@@ -19,7 +19,7 @@ function SignInButton() {
       <span className="absolute top-0 left-0 w-full h-full transition-all duration-300 delay-200 group-hover:h-0 ease rounded-lg" style={{ backgroundColor: 'var(--hover-bg-color)' }}></span>
       <span className="absolute bottom-0 left-0 w-full h-full transition-all duration-300 delay-200 group-hover:h-0 ease rounded-lg" style={{ backgroundColor: 'var(--hover-bg-color)' }}></span>
       <span className="absolute inset-0 w-full h-full duration-300 delay-300 opacity-0 group-hover:opacity-100" style={{ backgroundColor: 'var(--hover-bg-color)' }}></span>
-      <span className="relative transition-colors duration-300 delay-200 group-hover:text-white ease">Sign In</span>
+      <span className="relative transition-colors duration-300 delay-200 ease">Sign In</span>
     </button>
   );
 }
