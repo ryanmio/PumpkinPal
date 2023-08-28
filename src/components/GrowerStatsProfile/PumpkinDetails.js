cee653a22aa77c44c24d6fd5424fe0ddfbed6194
ryanmio <ryan@mioduski.us>
2023-08-27 20:56:25 -0400

    refactor pumpkin details component for DRY

--- src/components/GrowerStatsProfile/PumpkinDetails.js
+++ src/components/GrowerStatsProfile/PumpkinDetails.js
@@ -5,35 +5,37 @@ import { db } from '../../firebase';
 import Spinner from '../Spinner';
 import { UserContext } from '../../contexts/UserContext';
 
-// Conditionally renders fields based on their values
+const Field = ({ label, value, link }) => value && (
+  <p><b>{label}:</b> {link ? <Link to={link} className="text-current no-underline hover:underline">{value}</Link> : value}</p>
+);
+
 const PumpkinDetailsCard = ({ data }) => (
   <div className="bg-white shadow rounded-lg p-4 mb-4">
     <h1>{data.id} {data.name}</h1>
-    {data.grower && <p><b>Grower:</b> <Link to={`/grower/${encodeURIComponent(data.grower)}`} className="text-current no-underline hover:underline">{data.grower}</Link></p>}
-    {(data.ott !== null && data.ott !== 0) && <p><b>OTT:</b> {data.ott}</p>}
-    <p><b>Weight:</b> {data.weight}</p>
-    {(typeof data.seed !== 'undefined' && !isNaN(data.seed)) && <p><b>Seed:</b> {data.seed}</p>}
-    {(typeof data.pollinator !== 'undefined' && !isNaN(data.pollinator)) && <p><b>Pollinator:</b> {data.pollinator}</p>}
-    <p><b>Year:</b> {data.year}</p>
-    {data.state && <p><b>State:</b> {data.state}</p>}
-    {data.contestName && <p><b>Site:</b> <Link to={`/site-profile/${data.contestName.replace(/ /g, '_')}`} className="text-current no-underline hover:underline">{data.contestName}</Link></p>}
+    <Field label="Grower" value={data.grower} link={`/grower/${encodeURIComponent(data.grower)}`} />
+    <Field label="OTT" value={data.ott} />
+    <Field label="Weight" value={data.weight} />
+    <Field label="Seed" value={data.seed} />
+    <Field label="Pollinator" value={data.pollinator} />
+    <Field label="Year" value={data.year} />
+    <Field label="State" value={data.state} />
+    <Field label="Site" value={data.contestName} link={`/site-profile/${data.contestName.replace(/ /g, '_')}`} />
   </div>
 );
 
-// Component for displaying pumpkin rankings
 const PumpkinRankingsCard = ({ data }) => (
   <div className="bg-white shadow rounded-lg p-4 mb-4 flex justify-center">
     <div className="max-w-lg w-full mb-4">
       <div className="grid grid-cols-3 gap-4 text-center">
-        <div className="col-span-3 text-lg font-bold mb-2">Ranking Matrix</div> {/* Heading */}
-        <div></div> {/* Empty cell */}
+        <div className="col-span-3 text-lg font-bold mb-2">Ranking Matrix</div>
+        <div></div>
         <div><b>{data.year}</b></div>
         <div><b>All Time</b></div>
         <div><b>Global</b></div>
-        <div>#{data.yearGlobalRank}</div> 
+        <div>#{data.yearGlobalRank}</div>
         <div>#{data.lifetimeGlobalRank}</div>
         <div><b>{data.country}</b></div>
-        <div>#{data.yearlyCountryRank}</div> 
+        <div>#{data.yearlyCountryRank}</div>
         <div>#{data.lifetimeCountryRank}</div>
         <div><b>{data.state}</b></div>
         <div>#{data.yearlyStateRank}</div>
@@ -43,47 +45,32 @@ const PumpkinRankingsCard = ({ data }) => (
   </div>
 );
 
-// Component for fetching and displaying pumpkin details and rankings
 const PumpkinDetails = () => {
   const { id } = useParams();
-  const [pumpkinData, setPumpkinData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [state, setState] = useState({ loading: true, error: null, data: null });
   const navigate = useNavigate();
   const { user } = useContext(UserContext);
 
-  // Fetch data on component mount
   useEffect(() => {
     const fetchData = async () => {
       try {
         const docRef = doc(db, 'Stats_Pumpkins', id);
         const docSnap = await getDoc(docRef);
         if (docSnap.exists()) {
-          setPumpkinData(docSnap.data());
+          setState({ loading: false, data: docSnap.data(), error: null });
         } else {
-          setError("No such pumpkin!");
+          setState({ loading: false, data: null, error: "No such pumpkin!" });
         }
       } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
+        setState({ loading: false, data: null, error: error.message });
       }
     };
-
     fetchData();
-  }, [id]); // Depend on 'id' so the effect runs whenever it changes
+  }, [id]);
 
-  // Loading state
-  if (loading) {
-    return <Spinner />;
-  }
+  if (state.loading) return <Spinner />;
+  if (state.error) return <div>Error: {state.error}</div>;
 
-  // Error state
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
-  // Render fetched data
   return (
     <div className="min-h-screen flex justify-start flex-col">
       <div className="container mx-auto px-4 pt-10 flex flex-col space-y-4">
@@ -92,8 +79,8 @@ const PumpkinDetails = () => {
             <button onClick={() => navigate(-1)} className="text-gray-700 hover:text-gray-900 transition duration-150 ease-in-out">← Back</button>
           )}
         </div>
-        <PumpkinDetailsCard data={pumpkinData} />
-        <PumpkinRankingsCard data={pumpkinData} />
+        <PumpkinDetailsCard data={state.data} />
+        <PumpkinRankingsCard data={state.data} />
       </div>
     </div>
   );
