480bcf954a58a071eb27ec601a6e2b7090fba718
ryanmio <ryan@mioduski.us>
2023-08-31 18:28:51 -0400

    Update ImageCard.js

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -294,7 +294,7 @@ return (
     <Modal isOpen={isModalOpen} onRequestClose={closeModal} className="flex flex-col items-center justify-center bg-white rounded-lg p-4 max-w-lg mx-auto mt-20">
       <button onClick={closeModal} className="self-start text-xl font-bold">&times;</button>
       {isLoading ? (
-        <Spinner /> // Show spinner while loading
+        <Spinner />
       ) : (
         <img src={selectedImage} alt="Selected" className="max-w-full max-h-64 object-contain" />
       )}
