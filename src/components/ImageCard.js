3b64ad8bdbf6bfa09a92b157f9bc95ca01a81176
ryanmio <ryan@mioduski.us>
2023-08-31 08:24:25 -0400

    Update ImageCard.js

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -260,20 +260,16 @@ return (
 </div>
 
     </div>
-    <Modal isOpen={isModalOpen} onRequestClose={closeModal} className="flex flex-col items-center justify-center bg-white rounded-lg p-4 max-w-lg mx-auto mt-20">
-      <button onClick={closeModal} className="self-start text-xl font-bold">&times;</button>
-      {isLoading ? (
-        <Spinner />
-      ) : (
-        <img src={selectedImage} alt="Selected" className="max-w-full max-h-64 object-contain" />
-      )}
-      <div className="flex space-x-4 mt-4">
-        <Button onClick={handleShare}>Share to Facebook</Button>
-        <Button onClick={handleDownload}>Download</Button>
-        <Button onClick={handleDelete}>Delete</Button>
-      </div>
-    </Modal>
+<Modal isOpen={isModalOpen} onRequestClose={closeModal} className="flex flex-col items-center justify-center bg-white rounded-lg p-4 max-w-lg mx-auto mt-20">
+  <button onClick={closeModal} className="self-start text-xl font-bold">&times;</button>
+  {isLoading ? <Spinner /> : <img src={selectedImage} alt="Selected" className="max-w-full max-h-64 object-contain" />}
+  <div className="flex space-x-4 mt-4">
+    <Button onClick={handleShare}>Share to Facebook</Button>
+    <Button onClick={handleDownload}>Download</Button>
+    <Button onClick={handleDelete}>Delete</Button>
   </div>
+</Modal>
+</div>
 );
 };
 
