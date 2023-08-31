dc022ed360110d77968d6d4b434af56f91cedc7f
ryanmio <ryan@mioduski.us>
2023-08-31 19:43:41 -0400

    Update AddMeasurement.js

--- src/components/AddMeasurement.js
+++ src/components/AddMeasurement.js
@@ -155,11 +155,13 @@ const addMeasurement = async (e) => {
             max={999}
             value={circumference} 
           />
+           <div className="flex flex-col">
           <DateInput 
             id="measurementDate"
             selected={measurementDate}
             onChange={(date) => setMeasurementDate(date)} 
           />
+        </div>
          <div className="flex justify-between items-center mt-4">
           <button type="button" onClick={() => navigate('/dashboard')} className="text-blue-600 hover:underline">Cancel</button>
           <Button 
