6214abf72e0d859335568369dc893b0fb1f76ca5
ryanmio <ryan@mioduski.us>
2023-09-02 17:58:46 -0400

    Update ImageGallery.js

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -9,10 +9,10 @@ const ImageGallery = ({ images, openModal, handleUpload }) => {
     <div className="grid grid-cols-2 gap-4">
       {images.map((imageObj, index) => (
         <div key={index} onClick={() => openModal(imageObj.original)} className="w-full aspect-ratio-square relative">
-          <img src={imageObj.thumbnail} alt="Preview" className="w-full h-full object-cover absolute top-0 left-0" loading="lazy" />
+          <img src={imageObj.thumbnail} alt="Preview" className="w-full h-full rounded-lg object-cover absolute top-0 left-0" loading="lazy" />
         </div>
       ))}
-      <div {...getRootProps()} className="w-full flex justify-center items-center border-2 border-dashed border-gray-400 rounded cursor-pointer hover:bg-gray-100 aspect-ratio-square relative">
+      <div {...getRootProps()} className="w-full flex justify-center items-center border-2 border-dashed border-gray-400 rounded-lg cursor-pointer hover:bg-gray-100 aspect-ratio-square relative">
         <input {...getInputProps()} className="hidden" />
         <PlusIcon className="h-8 w-8 text-gray-400" />
       </div>
