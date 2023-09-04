e416bcff94f637e9ea182721212b5b723d0c4b5d
ryanmio <ryan@mioduski.us>
2023-09-04 14:39:02 -0400

    Update MeasurementsCard.js

--- src/components/MeasurementsCard.js
+++ src/components/MeasurementsCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { doc, deleteDoc } from 'firebase/firestore';
 import { auth, db } from '../firebase';
