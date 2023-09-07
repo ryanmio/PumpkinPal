2dbdb9eafbcff94593b32acc00a70842c0d9c189
ryanmio <ryan@mioduski.us>
2023-09-06 20:28:33 -0400

    mission statement

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -20,6 +20,14 @@ export default function Homepage() {
     </div>
 </div>
 
+    {/* Mission Statement Section */}
+    <div className="w-full px-8 py-8">
+        <h2 className="text-2xl font-bold mb-4">🌱 Our Mission</h2>
+        <p className="text-lg mb-6">Our mission is to empower the pumpkin growing community by providing a tool that makes the hobby more accessible and enjoyable. We envision a future where every weigh-off is crowded with more and heavier pumpkins, and we're committed to making that vision a reality through the continuous development and improvement of PumpkinPal.</p>
+        <h2 className="text-2xl font-bold mb-4">💸 Always Free</h2>
+        <p className="text-lg mb-6">PumpkinPal is committed to remaining free for all users. We believe in the power of community and the spirit of sharing knowledge and resources. As such, we pledge to keep PumpkinPal free to use, now and always.</p>
+    </div>
+
     {/* Features Section */}
     <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-4 w-full px-4 sm:px-8 -mx-4 sm:-mx-8 mb-8">
     {[
