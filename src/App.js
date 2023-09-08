dff9fa81a26275f2513d5d2626906764390a9eb3
ryanmio <ryan@mioduski.us>
2023-09-07 20:38:59 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'chart.js/auto';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
