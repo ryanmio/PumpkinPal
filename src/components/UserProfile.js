ff84c66213140aaaa706d759cd2023c7ddb8bb0f
ryanmio <ryan@mioduski.us>
2023-09-24 10:23:36 -0400

    dark mode select on user profile

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -15,6 +15,7 @@ function UserProfile() {
   const [preferredUnit, setPreferredUnit] = useState('in');
   const [showDeleteModal, setShowDeleteModal] = useState(false);
   const navigate = useNavigate();
+  const { colorMode, setColorMode } = useDarkMode();
     
     const confirmDeleteAccount = async () => {
     try {
@@ -174,29 +175,37 @@ const handleLogout = () => {
       <Toaster />
       <h2 className="text-2xl font-bold mb-2 text-center pt-4 pb-2">User Profile</h2>
       <div className="grid gap-8 md:grid-cols-2">
-        <div className="bg-white shadow overflow-hidden rounded-lg p-4">
-          <h3 className="text-lg leading-6 font-medium text-gray-900 mb-4 text-left">Account Information</h3>
-          <form onSubmit={updatePreferences} className="space-y-4 text-left">
-            <div>
-              <label className="block text-sm font-medium text-gray-700">
-                Email
-              </label>
-              <input type="email" value={auth.currentUser.email} readOnly className="mt-1 w-full p-2 border-2 border-gray-300 bg-gray-100 rounded" />
-            </div>
-            <div>
-              <label className="block text-sm font-medium text-gray-700">
-                Preferred Measurement Unit
-              </label>
-              <select value={preferredUnit} onChange={e => setPreferredUnit(e.target.value)} className="mt-1 w-full p-2 border-2 border-gray-300 rounded">
-                <option value="cm">cm</option>
-                <option value="in">in</option>
-              </select>
-            </div>
-            <div className="text-right">
-              <button type="submit" className="green-button inline-flex items-center justify-center px-2 py-1 border text-sm font-medium rounded-md shadow-sm text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">Save</button>
-            </div>
-          </form>
+      <div className="bg-white shadow overflow-hidden rounded-lg p-4">
+      <h3 className="text-lg leading-6 font-medium text-gray-900 mb-4 text-left">Account Information</h3>
+      <form onSubmit={updatePreferences} className="space-y-4 text-left">
+        <div>
+          <label className="block text-sm font-medium text-gray-700">
+            Email
+          </label>
+          <input type="email" value={auth.currentUser.email} readOnly className="mt-1 w-full p-2 border-2 border-gray-300 bg-gray-100 rounded" />
         </div>
+        <div>
+          <label className="block text-sm font-medium text-gray-700">
+            Preferred Measurement Unit
+          </label>
+          <select value={preferredUnit} onChange={e => setPreferredUnit(e.target.value)} className="mt-1 w-full p-2 border-2 border-gray-300 rounded">
+            <option value="cm">cm</option>
+            <option value="in">in</option>
+          </select>
+        </div>
+        <div>
+          <label className="block text-sm font-medium text-gray-700">
+            Dark Mode
+          </label>
+          <button onClick={() => setColorMode(colorMode === 'dark' ? 'light' : 'dark')}>
+            Toggle color mode
+          </button>
+        </div>
+        <div className="text-right">
+          <button type="submit" className="green-button inline-flex items-center justify-center px-2 py-1 border text-sm font-medium rounded-md shadow-sm text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">Save</button>
+        </div>
+      </form>
+    </div>
         
         <div className="bg-white shadow overflow-hidden rounded-lg p-4">
           <h3 className="text-lg leading-6 font-medium text-gray-900 mb-4 text-left">Change Password</h3>
