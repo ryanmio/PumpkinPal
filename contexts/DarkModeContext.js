009a1197ec3c06a18ea064599fffcdff6760ec28
ryanmio <ryan@mioduski.us>
2024-02-24 16:29:07 -0500

    move contexts

---
+++ contexts/DarkModeContext.js
@@ -0,0 +1,42 @@
+'use client';
+import React, { createContext, useContext, useEffect, useState } from 'react';
+
+const DarkModeContext = createContext();
+
+export function DarkModeProvider({ children }) {
+    const [colorMode, rawSetColorMode] = useState(undefined);
+
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            const root = window.document.documentElement;
+            const initialColorValue = root.style.getPropertyValue('--initial-color-mode');
+            rawSetColorMode(initialColorValue);
+        }
+    }, []);
+
+    const setColorMode = newValue => {
+        if (typeof window !== "undefined") {
+            const root = window.document.documentElement;
+            localStorage.setItem('color-mode', newValue);
+
+            // Set data-theme attribute
+            root.setAttribute('data-theme', newValue);
+
+            rawSetColorMode(newValue);
+        }
+    };
+
+    return (
+        <DarkModeContext.Provider value={{ colorMode, setColorMode }}>
+            {children}
+        </DarkModeContext.Provider>
+    );
+}
+
+export function useDarkMode() {
+    const context = useContext(DarkModeContext);
+    if (!context) {
+        throw new Error('useDarkMode must be used within a DarkModeProvider');
+    }
+    return context;
+}
---
+++ contexts/GrowerContext.js
@@ -0,0 +1,51 @@
+'use client';
+import React, { createContext, useState, useEffect } from 'react';
+import { toast } from 'react-hot-toast';
+import { db } from '../firebase'; // Ensure this path is correct based on your project structure
+import { doc, getDoc } from 'firebase/firestore';
+import fetchPumpkins from '../app/utilities/fetchPumpkins'; // Ensure this path is correct based on your project structure
+
+export const GrowerContext = createContext();
+
+export const GrowerContextProvider = ({ children }) => {
+  const [growerName, setGrowerName] = useState(null);
+  const [growerData, setGrowerData] = useState(null);
+  const [pumpkins, setPumpkins] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchGrowerData = async () => {
+      setLoading(true);
+      try {
+        const growerDocRef = doc(db, 'Stats_Growers', growerName);
+        const growerDoc = await getDoc(growerDocRef);
+        if (!growerDoc.exists()) {
+          throw new Error(`No grower found with the ID "${growerName}".`);
+        }
+        const growerData = growerDoc.data();
+        setGrowerData(growerData);
+        setLoading(false);
+
+        // Fetch pumpkins using fetchPumpkins function
+        const fetchedPumpkins = await fetchPumpkins(growerName);
+        setPumpkins(fetchedPumpkins);
+      } catch (error) {
+        console.error("Error Detail:", error);
+        setError(error.message);
+        toast.error(error.message);
+        setLoading(false);
+      }
+    };
+
+    if (growerName) {
+      fetchGrowerData();
+    }
+  }, [growerName]);
+
+  return (
+    <GrowerContext.Provider value={{ growerName, setGrowerName, growerData, pumpkins, loading, error }}>
+      {children}
+    </GrowerContext.Provider>
+  );
+};
