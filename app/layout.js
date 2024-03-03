d53ef013da8865aadbd97402e2f1b871fc51e6c8
ryanmio <ryan@mioduski.us>
2024-03-03 10:24:10 -0500

    Integrate Google Analytics with Next.js 14
    This commit introduces Google Analytics (GA) integration into our Next.js application, leveraging the new `@next/third-parties/google` package for optimized third-party script loading. By utilizing the `GoogleAnalytics` component, we ensure that GA tracking is efficiently included across all routes in the application.
    
    Key Changes:
    - Added `GoogleAnalytics` component to the `RootLayout` to enable global GA tracking with the measurement ID provided via environment variable `NEXT_PUBLIC_TRACKING_ID`.
    - Ensured automatic tracking of pageviews on client-side navigations by enabling “Enhanced Measurement” in the GA Admin panel.
    - Installed `next/third-parties@latest` and `next@latest` packages to support the integration.
    
    This setup aligns with Next.js 14's recommendations for third-party script inclusion, ensuring our application benefits from the latest performance optimizations and tracking capabilities.

--- app/layout.js
+++ app/layout.js
@@ -5,28 +5,31 @@ import "./globals.css";
 import { GrowerContextProvider } from '../contexts/GrowerContext';
 import { UserProvider } from '../contexts/UserContext';
 import { DarkModeProvider } from '../contexts/DarkModeContext';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 // Dynamically import the Sidebar component with SSR disabled
 const Sidebar = dynamic(() => import('../src/components/Sidebar'), { ssr: false });
-
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <head />
-      <body className={inter.className}>
-        <DarkModeProvider>
-          <UserProvider>
-            <GrowerContextProvider>
-              <Sidebar /> {/* Sidebar now manages its own open/close state */}
-              <div className="main-content flex-grow">
-                {children}
-              </div>
-            </GrowerContextProvider>
-          </UserProvider>
-        </DarkModeProvider>
-      </body>
-    </html>
+    <>
+      <html lang="en">
+        <head />
+        <body className={inter.className}>
+          <DarkModeProvider>
+            <UserProvider>
+              <GrowerContextProvider>
+                <Sidebar /> {/* Sidebar manages its own open/close state */}
+                <div className="main-content flex-grow">
+                  {children}
+                </div>
+              </GrowerContextProvider>
+            </UserProvider>
+          </DarkModeProvider>
+        </body>
+      </html>
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_TRACKING_ID} />
+    </>
   );
 }
