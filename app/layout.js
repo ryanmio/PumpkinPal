cf9263e91faa33068a5445757ca87a43ce54c8b1
ryanmio <ryan@mioduski.us>
2024-03-08 18:18:01 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 // Metadata for the page, to be managed with the Metadata API
 export const metadata = {
-  title: 'PumpkinPal: Your Pumpkin Growing Companion',
+  title: 'PumpkinPal: Your Pumpkin Growing Companion App',
   description: 'PumpkinPal is a user-friendly, open-source application designed for serious pumpkin growers. Calculate and track the weight of your pumpkins using the OTT method, manage your pumpkins and measurements, and view detailed data all in one place.',
   keywords: 'PumpkinPal, Pumpkin Growing, OTT Weight Calculation, Pumpkin Management, Measurement Management, Pumpkin Detail View, User Profile, Real-Time Updates, Data Backup',
   og: {
