25b52b30937a3e349af7b55501473ccc725ddea4
ryanmio <ryan@mioduski.us>
2024-03-10 12:04:53 -0400

    remove border from navbar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -26,7 +26,7 @@ function Sidebar() {
   };
 
   return <>
-    <nav className="fixed top-0 z-50 w-full border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+    <nav className="fixed top-0 z-50 w-full dark:bg-gray-800">
       <div className="flex items-center justify-between px-3 py-3 lg:px-5 lg:pl-3">
         <div className="flex items-center">
         {currentUser && (
