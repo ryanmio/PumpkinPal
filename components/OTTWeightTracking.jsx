58c8423ac13514e35fce5d9c787f39964cfc453e
ryanmio <ryan@mioduski.us>
2024-03-11 19:59:54 -0400

    ott graph update

---
+++ components/OTTWeightTracking.jsx
@@ -0,0 +1,93 @@
+'use client'
+import { ResponsiveLine } from "@nivo/line";
+import React from "react";
+
+function LineChart(props) {
+    return (
+      (<div {...props}>
+        <ResponsiveLine
+          data={[
+            {
+              id: "Desktop",
+              data: [
+                { x: "Jan", y: 43 },
+                { x: "Feb", y: 137 },
+                { x: "Mar", y: 61 },
+                { x: "Apr", y: 145 },
+                { x: "May", y: 26 },
+                { x: "Jun", y: 154 },
+              ],
+            },
+            {
+              id: "Mobile",
+              data: [
+                { x: "Jan", y: 60 },
+                { x: "Feb", y: 48 },
+                { x: "Mar", y: 177 },
+                { x: "Apr", y: 78 },
+                { x: "May", y: 96 },
+                { x: "Jun", y: 204 },
+              ],
+            },
+          ]}
+          margin={{ top: 10, right: 10, bottom: 40, left: 40 }}
+          xScale={{
+            type: "point",
+          }}
+          yScale={{
+            type: "linear",
+          }}
+          axisTop={null}
+          axisRight={null}
+          axisBottom={{
+            tickSize: 0,
+            tickPadding: 16,
+          }}
+          axisLeft={{
+            tickSize: 0,
+            tickValues: 5,
+            tickPadding: 16,
+          }}
+          colors={["#2563eb", "#e11d48"]}
+          pointSize={6}
+          useMesh={true}
+          gridYValues={6}
+          theme={{
+            tooltip: {
+              chip: {
+                borderRadius: "9999px",
+              },
+              container: {
+                fontSize: "12px",
+                textTransform: "capitalize",
+                borderRadius: "6px",
+              },
+            },
+            grid: {
+              line: {
+                stroke: "#f3f4f6",
+              },
+            },
+          }}
+          role="application" />
+      </div>)
+    );
+  }
+
+export default function OTTWeightTracking() {
+  return (
+    <section className="w-full py-12 md:py-24 lg:py-32 bg-[#f2f2f2] dark:bg-[#333]">
+      <div className="container px-4 md:px-6">
+        <div className="flex flex-col justify-center space-y-4">
+          <div className="space-y-2">
+            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl text-center">OTT Weight Tracking</h2>
+            <p className="max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400 text-center mx-auto">
+              Estimate the weight of your pumpkins using the Over The Top (OTT) formula and track the progress over time.
+            </p>
+          </div>
+          <LineChart className="w-full aspect-[16/9]" />
+        </div>
+      </div>
+    </section>
+  );
+}
