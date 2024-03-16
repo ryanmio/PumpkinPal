1a1974f1f787b7101a79c6600290975168ce8058
ryanmio <ryan@mioduski.us>
2024-03-16 17:59:32 -0400

    style search bar

--- app/search/page.js
+++ app/search/page.js
@@ -37,6 +37,7 @@ const RightArrowIcon = () => (
   </svg>
 );
 
+// Modify the Hit component
 const Hit = ({ hit }) => {
   const router = useRouter();
   const { setGrowerName } = useContext(GrowerContext);
@@ -170,12 +171,22 @@ const SearchPage = () => {
         <Configure hitsPerPage={10} />
         <div className="p-4 flex justify-center">
           <div className="flex items-center bg-white rounded-lg shadow-md p-2 mx-auto max-w-lg w-full">
+            <svg
+              className="h-6 w-6 text-gray-500 ml-4"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
             <SearchBox
-              translations={{
-                placeholder: 'Search for a pumpkin grower...',
-              }}
-              submit={<></>}
-              reset={<></>}
+              placeholder="Search for growers, pumpkins, or sites..."
             />
           </div>
         </div>
