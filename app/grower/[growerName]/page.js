6b56ca337b7d92b9a63dee4ab9ebe94026363778
ryanmio <ryan@mioduski.us>
2024-03-20 12:00:00 -0400

    add metatags to grower page

--- app/grower/[growerName]/page.js
+++ app/grower/[growerName]/page.js
@@ -1,13 +1,63 @@
 'use server'
-// app/grower/[growerName]/page.server.js
+// app/grower/[growerName]/page.js
 import React, { lazy, Suspense } from 'react';
 import Link from 'next/link';
-import admin from '../../../lib/firebaseAdmin'; // Adjust the import path as necessary
+import admin from '../../../lib/firebaseAdmin';
 import fetchPumpkins from '../../utilities/fetchPumpkins';
 
 const Header = lazy(() => import('./Header'));
 const SummarySection = lazy(() => import('./SummarySection'));
 const TableSection = lazy(() => import('./TableSection'));
+
+export async function generateMetadata({ params }) {
+  const growerName = decodeURIComponent(params.growerName);
+  const db = admin.firestore();
+  let metadata = {};
+
+  try {
+    const growerDoc = await db.collection('Stats_Growers').doc(growerName).get();
+    if (growerDoc.exists) {
+      const growerData = growerDoc.data();
+      const strippedRanking = growerData.globalRanking ? growerData.globalRanking.replace('Global: ', '') : '';
+      metadata = {
+        title: `${growerData.firstName} ${growerData.lastName} - #${strippedRanking} Global - Profile`,
+        description: `${growerData.firstName} ${growerData.lastName} GPC weigh-off history on PumpkinPal`,
+        openGraph: {
+          title: `${growerData.firstName} ${growerData.lastName} - #${strippedRanking} Global - Profile`,
+          description: `${growerData.firstName} ${growerData.lastName} GPC weigh-off history on PumpkinPal`,
+          images: [
+            {
+              url: '%PUBLIC_URL%/images/metashare.png',
+              width: 1200,
+              height: 630,
+              alt: `${growerData.firstName} ${growerData.lastName} Profile`,
+            },
+          ],
+        },
+      };
+    }
+  } catch (error) {
+    console.error('Error fetching grower data for metadata:', error);
+    metadata = {
+      title: 'Grower Profile - PumpkinPal',
+      description: 'Explore competitive pumpkin grower profiles on PumpkinPal.',
+      openGraph: {
+        title: 'Grower Profile - PumpkinPal',
+        description: 'Explore competitive pumpkin grower profiles on PumpkinPal',
+        images: [
+          {
+            url: '%PUBLIC_URL%/images/metashare.png',
+            width: 1200,
+            height: 630,
+            alt: 'PumpkinPal Grower Profile',
+          },
+        ],
+      },
+    };
+  }
+
+  return metadata;
+}
 
 export default async function GrowerStatsProfile({ params }) {
   // Decode the growerName parameter
